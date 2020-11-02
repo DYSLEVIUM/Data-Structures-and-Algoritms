3b31ad9d1005fdf6a1f1d6bde710b5ddc9f38c28
Pushpakant Behera <38003829+Pushpakant@users.noreply.github.com>
2020-11-02 18:51:22 +0545

    Create findAllDuplicates.cc

---
+++ leetcodeSolutions/findAllDuplicates.cc
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+        
+        for(int i=0;i<nums.size();++i){
+            if(nums[abs(nums[i])-1]<0)
+                res.emplace_back(abs(nums[i]));
+            
+            nums[abs(nums[i])-1] *= -1;
+        }
+        
+        return res;
+    }
+};
