3bda5d37e45246ba2c83dc96131995bb3d8ff5a1
Pushpakant Behera <38003829+Pushpakant@users.noreply.github.com>
2020-11-02 18:44:09 +0545

    Create findTheDuplicate.cc

---
+++ leetcodeSolutions/findTheDuplicate.cc
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/find-the-duplicate-number/solution/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[abs(nums[i]) - 1] < 0)
+                return abs(nums[i]);
+            else
+                nums[abs(nums[i]) - 1] *= -1;
+        }
+        return -1;
+    }
+};
