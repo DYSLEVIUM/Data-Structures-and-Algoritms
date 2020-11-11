7456566aef640031cf4bcdb79dda149fdaf523c8
Pushpakant Behera <38003829+Pushpakant@users.noreply.github.com>
2020-11-11 15:21:48 +0545

    made some refactoring

--- dataStructures/linkedList.h
+++ dataStructures/linkedList.h
@@ -2,10 +2,9 @@ template <typename T>
 class List {
    private:
     ListNode<T>* head;
-    ListNode<T>* temp;
 
    public:
-    List() : head(nullptr), temp(nullptr) {}
+    List() : head(nullptr) {}
 
     bool empty() {
         return this->head == nullptr;
@@ -24,15 +23,19 @@ class List {
     T back() {
         try {
             if (this->empty()) throw true;
-            if (this->head->next == nullptr) return this->head->data;
 
-            this->temp = this->head;
+            ListNode<T>* currNode = this->head;
 
-            while (this->temp->next != nullptr) {
-                this->temp = this->temp->next;
+            while (currNode->next != nullptr) {  //  traversing through the list
+                currNode = currNode->next;
             }
 
-            return this->temp->data;
+            T temp = currNode->data;  //  storing value to return it
+
+            delete currNode;
+            currNode = nullptr;
+
+            return temp;
         } catch (bool err) {
             cout << "\nList is empty.\n";
             return T(-1);
@@ -40,7 +43,7 @@ class List {
     }
 
     void push_front(T da) {
-        ListNode<T>* newNode = new ListNode<T>(da);
+        ListNode<T>* newNode = new ListNode<T>(da);  //  making a new node
 
         newNode->next = this->head;
         this->head = newNode;
@@ -53,12 +56,15 @@ class List {
         if (this->empty()) {  //  we can't access pointer if it is nullptr
             this->head = newNode;
         } else {
-            this->temp = this->head;
-            while (this->temp->next != nullptr) {
-                this->temp = this->temp->next;
+            ListNode<T>* currNode = this->head;
+
+            while (currNode->next != nullptr) {
+                currNode = currNode->next;
             }
 
-            this->temp->next = newNode;
+            currNode->next = newNode;
+
+            currNode = nullptr;
         }
     }
 
@@ -66,11 +72,11 @@ class List {
         try {
             if (this->empty()) throw true;
 
-            this->temp = this->head;
+            ListNode<T>* temp = this->head;
             this->head = this->head->next;
 
-            this->temp = nullptr;
-            delete this->temp;
+            delete temp;
+            temp = nullptr;
         } catch (bool err) {
             cout << "List is empty.\n";
         }
@@ -86,35 +92,51 @@ class List {
                 currNode = currNode->next;
             }
 
-            currNode->next = nullptr;
             delete currNode->next;
+            currNode->next = nullptr;
         } catch (bool err) {
             cout << "\nList is empty.\n";
         }
     }
 
-    void insert() {}
-    void erase() {}
-    void reverse() {}
+    // void insert(int pos, T da) {
+    //     ListNode<T>* newNode = new ListNode<T>(da);
+
+    //     this->temp = this->head;
+
+    //     while (pos--) {
+    //         this->temp = this->temp->next;
+    //     }
+
+    //     newNode->next = this->temp->next;
+
+    //     this->temp->next = newNode;
+    // }
+    // void erase() {}
+    // void reverse() {}
+
+    // ListNode<T>* getHead() {
+    //     return this->head;
+    // }
+
+    void display() {
+        ListNode<T>* currNode = this->head;
+        while (currNode != nullptr) {
+            cout << currNode->data << " -> ";
+            currNode = currNode->next;
+        }
+        cout << "nullptr\n";
+    }
 
     ~List() {
         if (!this->empty()) {
-            ListNode<T>* currNode = this->head;
-            ListNode<T>* prevNode;
+            while (this->head != nullptr) {
+                ListNode<T>* currNode = this->head;
+                this->head = this->head->next;
 
-            while (currNode != nullptr) {
-                prevNode = currNode;
-                currNode = currNode->next;
+                delete currNode;
+                currNode = nullptr;
             }
-
-            prevNode = nullptr;
-            currNode = nullptr;
-
-            delete currNode;
-            delete prevNode;
         }
-
-        delete this->head;
-        delete this->temp;
     }
 };
