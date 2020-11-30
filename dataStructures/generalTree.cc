99af48874d3f130db278091f2a3a3907f61035d5
Pushpakant Behera <38003829+Pushpakant@users.noreply.github.com>
2020-11-30 15:08:15 +0545

    Update generalTree.cc

--- dataStructures/generalTree.cc
+++ dataStructures/generalTree.cc
@@ -1,12 +1,4 @@
-using namespace std;
-
-template <typename T>
-class TreeNode {
-   public:
-    T data;
-    vector<TreeNode*> children;
-
-    TreeNode(T da) : data(da) {}//Compile: g++ -g -Wshadow -Wall main.cpp -o a.exe -Ofast -Wno-unused-result
+//Compile: g++ -g -Wshadow -Wall main.cpp -o a.exe -Ofast -Wno-unused-result
 //Build: g++ -g -Wshadow -Wall main.cpp -o a.exe -D_GLIBCXX_DEBUG
 //Compile and run: g++ -g -Wshadow -Wall main.cpp -o a.exe -Ofast -Wno-unused-result && a.exe
 
@@ -157,32 +149,3 @@ inline void solve() {
     cout << "Level order traversal Before Mirroring\n";
     root->levelOrderTraversal(root);
 }
-
-    T getData() {
-        return this->data;
-    }
-
-    void levelOrderTraversal(TreeNode* root) {
-        if (root == nullptr) return;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int n = q.size();
-
-            while (n--) {
-                //  dequeue an item from queue and print it
-                TreeNode* p = q.front();
-                q.pop();
-                cout << p->getData() << ' ';
-
-                //  enqueue all children of the dequeued item
-                for (int i = 0; i < p->children.size(); ++i) {
-                    q.push(p->children[i]);
-                }
-            }
-            cout << '\n';
-        }
-    }
-};
