d94e24674d7540103916005bd3f07f79b7ed1e0b
Pushpakant Behera <38003829+Pushpakant@users.noreply.github.com>
2020-12-03 17:48:22 +0545

    Update 0-1-knapsackMemozized.cc

--- algorithms/dynamicProgramming/0-1-knapsackMemozized.cc
+++ algorithms/dynamicProgramming/0-1-knapsackMemozized.cc
@@ -49,12 +49,12 @@ int dp[100][100];
 inline int knapsack(int* wt, int* val, int w, int n) {
     if (n == 0 || w == 0) return 0;
 
-    if (dp[n][w] != -1) return dp[n][w];
+    if (dp[n - 1][w - 1] != -1) return dp[n - 1][w - 1];
 
     if (wt[n - 1] <= w) {
-        return dp[n][w] = max(val[n - 1] + knapsack(wt, val, w - wt[n - 1], n - 1), knapsack(wt, val, w, n - 1));
+        return dp[n - 1][w - 1] = max(val[n - 1] + knapsack(wt, val, w - wt[n - 1], n - 1), knapsack(wt, val, w, n - 1));
     } else {
-        return dp[n][w] = knapsack(wt, val, w, n - 1);
+        return dp[n - 1][w - 1] = knapsack(wt, val, w, n - 1);
     }
 }
 
