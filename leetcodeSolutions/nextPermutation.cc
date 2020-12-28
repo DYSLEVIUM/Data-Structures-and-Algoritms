1564b7795c4134d63f03b3ac1b7ccd747352a3d3
Pushpakant Behera <38003829+Pushpakant@users.noreply.github.com>
2020-12-28 15:19:11 +0545

    Create nextPermutation.cc

---
+++ leetcodeSolutions/nextPermutation.cc
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int inversionPoint =-1;
+        
+        for(int i=nums.size()-2;i>=0;--i){
+            if(nums[i]<nums[i+1]){
+                inversionPoint = i;
+                break;
+            }
+        }
+        
+        if(inversionPoint!=-1){
+            for(int i=nums.size()-1;i>=0;--i){
+                if(nums[i]>nums[inversionPoint]){
+                    swap(nums[i],nums[inversionPoint]);
+                    break;
+                }
+            }    
+        }
+        
+        reverse(nums.begin()+inversionPoint+1,nums.end());            
+    }
+};
