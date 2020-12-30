003978afa864c27926fc5b44223d1e2ddef653d4
Pushpakant Behera <38003829+Pushpakant@users.noreply.github.com>
2020-12-30 16:49:42 +0545

    Create commonElementsInSortedArray.cc

---
+++ solvedQuestions/array/commonElementsInSortedArray.cc
@@ -0,0 +1,133 @@
+//Compile: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result
+//Build: g++ -g -Wshadow -Wall main.cc -o a.exe -D_GLIBCXX_DEBUG
+//Compile and run: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result && a.exe
+
+#pragma GCC optimize("Ofast,fast-math")
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <forward_list>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<int, int> pii;
+typedef std::pair<long, long> pl;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vl;
+typedef std::vector<pii> vpii;
+typedef std::vector<pl> vpl;
+typedef std::vector<vi> vvi;
+typedef std::vector<vl> vvl;
+typedef std::map<int, int> mii;
+typedef std::priority_queue<int> pqd;
+typedef std::priority_queue<int, vi, std::greater<int>> pqi;
+
+#define eb emplace_back
+#define F first
+#define S second
+#define MOD (long long)1e9 + 7
+#define PI 3.14159265358979323846
+#define INF __builtin_inff()
+
+#define fo(i, n) for (ll i = 0; i < n; ++i)
+#define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
+#define allC(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define deb(x) cout << '\n' \
+                    << #x << " = " << x << '\n'
+#define sortall(x) sort(x.begin(), x.end())
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); ++it)
+#define ps(x, y) std::fixed << std::setprecision(y) << x
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mk(arr, n, type) type* arr = new type[n]
+
+std::mt19937_64 rng(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+inline void setup() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
+    freopen("input.txt", "r", stdin);
+#else
+#ifndef ONLINE_JUDGE  // runs automatically for supported online judges
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+#endif
+}
+
+inline void solve();
+
+int main(int argc, char* argv[]) {
+    setup();
+
+    ll t = 1;
+    // std::cin >> t;
+
+    while (t--) solve();
+
+    return 0;
+}
+
+using namespace std;
+
+//  https://practice.geeksforgeeks.org/problems/common-elements1132/1#
+
+vector<int> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) {
+    int l = 0, m = 0, n = 0;
+    set<int> commS;  //  using set so that the repeated elements in one array does not get put twice
+
+    while (l < n1 && m < n2 && n < n3) {
+        if (A[l] == B[m] && A[l] == C[n]) {
+            commS.insert(A[l]);
+            ++l, ++m, ++n;
+        } else if (A[l] < B[m]) {
+            ++l;
+        } else if (B[m] < C[n]) {
+            ++m;
+        } else {
+            ++n;
+        }
+    }
+
+    return vector<int>(commS.begin(), commS.end());  //  returning a vector from set
+}
+
+inline void solve() {
+    int n1 = 6, n2 = 5, n3 = 8;
+    int A[] = {1, 5, 10, 20, 40, 80};
+    int B[] = {6, 7, 20, 80, 100};
+    int C[] = {3, 4, 15, 20, 30, 70, 80, 120};
+
+    vector<int> comm = commonElements(A, B, C, n1, n2, n3);
+
+    tr(it, comm) {
+        cout << *it << ' ';
+    }
+}
