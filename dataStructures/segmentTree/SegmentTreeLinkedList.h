5f86ccfd0b9d905e44b8d392aec91c7e7866306b
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-01-24 02:23:10 +0545

    Update SegmentTreeLinkedList.h

--- dataStructures/segmentTree/SegmentTreeLinkedList.h
+++ dataStructures/segmentTree/SegmentTreeLinkedList.h
@@ -32,18 +32,50 @@ class SegmentTree {
     void update(T index, T val) {
         if (this->leftmost == this->rightmost) {
             //  leaf
-            sum = val;
-        } else {
-            // have two children
-            if (index <= this->leftChild->rightmost) {
-                this->leftChild->update(index, val);
-            } else {
-                this->rightChild->update(index, val);
-            }
-
-            // recalculating sum after update
-            this->sum = this->leftChild->sum + this->rightChild->sum;
+            this->sum = val;
+            return;
         }
+
+        // have two children
+        if (index <= this->leftChild->rightmost) {
+            this->leftChild->update(index, val);
+        } else {
+            this->rightChild->update(index, val);
+        }
+
+        // recalculating sum after update
+        this->sum = this->leftChild->sum + this->rightChild->sum;
+    }
+
+    void update(T startIdx, T endIdx, T val) {
+        if (this->leftmost == this->rightmost) {
+            //  leaf
+            this->sum = val;
+            return;
+        }
+
+        //  value of rightMost will be the upperbound in this subtree
+        if (startIdx <= this->leftChild->rightmost) {
+            this->leftChild->update(startIdx, endIdx, val);
+        }
+
+        //  value of leftMost will be the lowerbound in this subtree
+        if (startIdx >= this->rightChild->leftmost) {
+            this->rightChild->update(startIdx, endIdx, val);
+        }
+
+        //  value of rightMost will be the upperbound in this subtree
+        if (endIdx <= this->leftChild->rightmost) {
+            this->leftChild->update(startIdx, endIdx, val);
+        }
+
+        //  value of leftMost will be the lowerbound in this subtree
+        if (endIdx >= this->rightChild->leftmost) {
+            this->rightChild->update(startIdx, endIdx, val);
+        }
+
+        // recalculating sum after update
+        this->sum = this->leftChild->sum + this->rightChild->sum;
     }
 
     T rangeSum(T l, T r) {
