c56efe431bb67732bcc64eac674a5daa0350eadf
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-02-18 14:00:09 +0545

    Create convertStringToNum.cc

---
+++ solvedQuestions/recursion/convertStringToNum.cc
@@ -0,0 +1,56 @@
+#pragma GCC optimize("Ofast,fast-math")
+
+#include <bits/stdc++.h>
+
+#define deb(x) cout << '\n' \
+                    << #x << " = " << x << '\n'
+
+inline void setup() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
+    freopen("input.txt", "r", stdin);
+#else
+#ifndef ONLINE_JUDGE  // runs automatically for supported online judges
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+#endif
+#endif
+}
+
+inline void solve();
+
+int main(int argc, char* argv[]) {
+    setup();
+
+    long long t = 1;
+    // std::cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
+
+//Compile: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result
+//Build: g++ -g -Wshadow -Wall main.cc -o a.exe -D_GLIBCXX_DEBUG
+//Compile and run: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result && a.exe
+
+using namespace std;
+
+int convertStringToNum(string s) {
+    if (s.length() == 1) return s[0] - '0';
+    int t = s[0] - '0';
+
+    s.erase(s.begin(), s.begin() + 1);
+
+    return t * pow(10, s.length()) + convertStringToNum(s);
+}
+
+inline void solve() {
+    string s = "12345";
+
+    cout << convertStringToNum(s);
+}
