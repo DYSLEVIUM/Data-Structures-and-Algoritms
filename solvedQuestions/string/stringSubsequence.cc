ad3b7c8abc6aa75c831f3550a952ae7b7a825e1e
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-03-04 10:50:23 +0545

    Create stringSubsequence.cc

---
+++ solvedQuestions/string/stringSubsequence.cc
@@ -0,0 +1,66 @@
+#define _USE_MATH_DEFINES
+#pragma GCC optimize("Ofast,fast-math")
+
+#include <bits/stdc++.h>
+
+#define deb(x) cout << '\n' \
+                    << #x << " = " << x << '\n'
+
+inline void setup() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
+    freopen("input.txt", "r", stdin);
+#else
+#ifndef ONLINE_JUDGE  // runs automatically for supported online judges
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+#endif
+#endif
+}
+
+inline void solve();
+
+int main(int argc, char* argv[]) {
+    setup();
+
+    long long t = 1;
+    // std::cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
+
+//Compile: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result
+//Build: g++ -g -Wshadow -Wall main.cc -o a.exe -D_GLIBCXX_DEBUG
+//Compile and run: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result && ./a.exe
+
+using namespace std;
+
+void subsequence(string s, string o){
+	if(s.length()==0){
+		cout<<o<<' ';
+		return;
+	}
+
+	string s1 = o;
+	string s2 = o;
+
+	s2.push_back(s[0]);
+	s.erase(s.begin());
+
+	subsequence(s,s1);
+	subsequence(s,s2);
+}
+
+
+inline void solve() {
+	string s;
+	cin>>s;
+
+	subsequence(s,"");
+}
