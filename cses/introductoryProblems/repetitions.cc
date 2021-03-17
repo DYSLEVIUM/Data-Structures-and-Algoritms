b83cd7559a163f6b59dac1d2a224978ab1cee3e3
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-03-17 21:50:11 +0545

    Create repetitions.cc

---
+++ cses/introductoryProblems/repetitions.cc
@@ -0,0 +1,61 @@
+#define _USE_MATH_DEFINES
+#pragma GCC optimize("Ofast,fast-math")
+
+#include <bits/stdc++.h>
+
+#define deb(x) cout << '\n' \
+                    << #x << " = " << x << '\n'
+
+inline void setup() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
+    freopen("input.txt", "r", stdin);
+#else
+#ifndef ONLINE_JUDGE  // runs automatically for supported online judges
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+#endif
+#endif
+}
+
+inline void solve();
+
+int main(int argc, char* argv[]) {
+    setup();
+
+    long long t = 1;
+    // std::cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
+
+//Compile: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result
+//Build: g++ -g -Wshadow -Wall main.cc -o a.exe -D_GLIBCXX_DEBUG
+//Compile and run: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result && ./a.exe
+
+using namespace std;
+
+inline void solve() {
+    string s;
+    cin >> s;
+
+    long long ans = LONG_LONG_MIN;
+    for (long long i = 0; i < s.length(); ++i) {
+        long long cnt = 1;
+        char temp = s[i];
+
+        while (i + 1 < s.length() && s[i + 1] == temp) {
+            ++cnt;
+            ++i;
+        }
+        ans = max(ans, cnt);
+    }
+
+    cout << ans;
+}
