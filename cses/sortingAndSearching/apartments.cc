a23c76d8397a4a635cc796f29eaf52345dec1bff
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-03-23 13:47:52 +0545

    Create apartments.cc

---
+++ cses/sortingAndSearching/apartments.cc
@@ -0,0 +1,69 @@
+#define _USE_MATH_DEFINES
+#pragma GCC optimize("Ofast,fast-math")
+
+#include <bits/stdc++.h>
+
+#define deb(x) cout << '\n' \
+                    << #x << " = " << x << '\n'
+
+inline void setup() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
+    freopen("input.txt", "r", stdin);
+#else
+#ifndef ONLINE_JUDGE  // runs automatically for supported online judges
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+#endif
+#endif
+}
+
+inline void solve();
+
+int main(int argc, char* argv[]) {
+    setup();
+
+    long long t = 1;
+    // std::cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
+
+//Compile: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result
+//Build: g++ -g -Wshadow -Wall main.cc -o a.exe -D_GLIBCXX_DEBUG
+//Compile and run: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result && ./a.exe
+
+using namespace std;
+
+inline void solve() {
+    long long n, m, k;
+    cin >> n >> m >> k;
+
+    long long* a = new long long[n];
+    for (long long i = 0; i < n; ++i) cin >> a[i];
+    sort(a, a + n);
+
+    long long* b = new long long[m];
+    for (long long i = 0; i < m; ++i) cin >> b[i];
+    sort(b, b + m);
+
+    long long i = 0, j = 0;
+    long long ans = 0;
+    while (i < n && j < m) {
+        if (b[j] >= a[i] - k && b[j] <= a[i] + k) {
+            ++ans, ++i, ++j;
+        } else if (b[j] > a[i] + k) {
+            ++i;
+        } else if (b[j] < a[i] - k) {
+            ++j;
+        }
+    }
+
+    cout << ans;
+}
