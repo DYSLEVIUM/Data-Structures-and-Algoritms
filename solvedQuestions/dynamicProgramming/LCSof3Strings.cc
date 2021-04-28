7b35935f2ced634231a7019fc7a3b22f5846fdce
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-04-28 13:16:54 +0545

    Update LCSof3Strings.cc

--- solvedQuestions/dynamicProgramming/LCSof3Strings.cc
+++ solvedQuestions/dynamicProgramming/LCSof3Strings.cc
@@ -84,7 +84,7 @@ inline void solve() {
 
         for (ll i{}; i <= s1.length(); ++i) {
             for (ll j{}; j <= s2.length(); ++j) {
-                for (ll k = {}; k <= s3.length(); ++k) {
+                for (ll k{}; k <= s3.length(); ++k) {
                     if (i == 0 || j == 0 || k == 0)
                         dp[i][j][k] = 0;
                     else if (s1[i - 1] == s2[j - 1] && s3[k - 1] == s1[i - 1])
