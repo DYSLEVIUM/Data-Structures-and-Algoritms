d89af5efbc39c34cb773134e9d319ea41163f145
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-06-09 19:43:06 +0530

    Create minimumCoins.cc

---
+++ solvedQuestions/dynamicProgramming/minimumCoins.cc
@@ -0,0 +1,130 @@
+#define _USE_MATH_DEFINES
+#pragma GCC optimize("Ofast,fast-math,unroll-loops")
+
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<int, int> pii;
+typedef std::pair<long, long> pl;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vl;
+typedef std::vector<pii> vpii;
+typedef std::vector<pl> vpl;
+typedef std::vector<vi> vvi;
+typedef std::vector<vl> vvl;
+typedef std::map<int, int> mii;
+typedef std::priority_queue<int> pqd;
+typedef std::priority_queue<int, vi, std::greater<int>> pqi;
+
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+#define MOD (long long)1e9 + 7
+#define PI 3.14159265358979323846
+#define INF __builtin_inff()
+
+#define fo(i, n) for (ll i = 0; i < n; ++i)
+#define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
+#define allC(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define deb(x) std::cout << '\n' \
+                         << #x << " = " << x << '\n'
+#define sortall(x) sort(x.begin(), x.end())
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); ++it)
+#define ps(x, y) std::fixed << std::setprecision(y) << x
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mk(arr, n, type) type* arr = new type[n]
+
+std::mt19937_64 rng(
+    std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+inline void setup() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+
+#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
+  freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+#endif
+}
+
+inline void solve();
+
+int main(int argc, char* argv[]) {
+  setup();
+
+  ll t = 1;
+  // std::cin >> t;
+
+  while (t--) solve();
+
+  return 0;
+}
+
+using namespace std;
+//  Compile and run: g++ -g -Wshadow -Wall practice.cpp -DLOCAL_PROJECT -o a -Ofast -Wno-unused-result && ./a
+
+inline void solve() {
+  int coins[] = {9, 2, 11, 5, 14, 17, 8, 18}, M = 8, V = 39;
+
+  int* dp = new int[V + 1];
+
+  fo(i, V + 1) dp[i] = INT32_MAX - 1;
+  dp[0] = 0;
+
+  //  iterative
+  auto recur = [&coins, &dp](int M, int V, auto&& recur) {
+    if (dp[V] != INT32_MAX - 1) return dp[V];
+
+    for (int i = 0; i < M; ++i) {
+      if (V - coins[i] >= 0) {
+        dp[V] = min(dp[V], 1 + recur(M, V - coins[i], recur));
+      }
+    }
+
+    return dp[V];
+  };
+
+  //  recursive
+  for (int i = 1; i < V + 1; ++i) {
+    for (int j = 0; j < M; ++j) {
+      if (i - coins[j] >= 0) {
+        dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
+      }
+    }
+  }
+
+  // int ans = recur(M, V, recur);
+  int ans = dp[V];
+
+  if (ans == INT32_MAX - 1)
+    cout << -1;
+  else
+    cout << ans;
+}
