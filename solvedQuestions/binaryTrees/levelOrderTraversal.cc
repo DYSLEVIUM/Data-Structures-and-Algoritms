a12f9580bc05a5ac4f063e42ff1e092fbddcfa5d
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-06-17 15:08:05 +0530

    Create levelOrderTraversal.cc

---
+++ solvedQuestions/binaryTrees/levelOrderTraversal.cc
@@ -0,0 +1,30 @@
+class Solution
+{
+    public:
+    //Function to return the level order traversal of a tree.
+    vector<int> levelOrder(Node* node)
+    {
+      //Your code here
+      
+      vector<int> res;
+      
+      if(node==NULL) return res;
+      
+      queue<Node*> q;
+      
+      q.push(node);k
+      
+      while(!q.empty()){
+        Node* f = q.front();
+        q.pop();
+
+        res.push_back(f->data);
+        
+        if(f->left!=NULL) q.push(f->left);
+     
+        if(f->right!=NULL) q.push(f->right);
+      }
+      
+      return res;
+    }
+};
