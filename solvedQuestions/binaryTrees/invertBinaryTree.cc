5e661f7e242acb17eed94341a5709e11dfd3cfba
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-06-17 16:43:47 +0530

    Create invertBinaryTree.cc

---
+++ solvedQuestions/binaryTrees/invertBinaryTree.cc
@@ -0,0 +1,136 @@
+#define _USE_MATH_DEFINES
+#pragma GCC optimize("Ofast,fast-math,unroll-loops")
+
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<ll, ll> pl;
+typedef std::vector<ll> vl;
+typedef std::vector<pl> vpl;
+typedef std::vector<vl> vvl;
+typedef std::map<ll, ll> mll;
+typedef std::priority_queue<ll> pqd;
+typedef std::priority_queue<ll, vl, std::greater<ll>> pqi;
+
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+#define MOD (ll)(1e9 + 7)
+#define PI 3.14159265358979323846
+#define INF __builtin_inff()
+
+#define fo(i, n) for (ll i = 0; i < n; ++i)
+#define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
+#define allC(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define deb(x) std::cout << '\n' \
+                         << #x << " = " << x << '\n'
+#define sortall(x) sort(x.begin(), x.end())
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); ++it)
+#define ps(x, y) std::fixed << std::setprecision(y) << x
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mk(arr, n, type) type* arr = new type[n]
+
+std::mt19937_64 rng(
+    std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+inline void setup() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+
+#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+
+inline void solve();
+
+int main(int argc, char* argv[]) {
+  setup();
+
+  ll t = 1;
+  // std::cin >> t;
+
+  while (t--) solve();
+
+  return 0;
+}
+
+using namespace std;
+//  Compile and run: g++ -g -Wshadow -Wall practice.cpp -DLOCAL_PROJECT -o a -Ofast -Wno-unused-result && ./a
+
+inline void solve() {
+  class Node {
+   public:
+    ll data;
+    Node* left;
+    Node* right;
+
+    Node(ll x) : data(x), left(nullptr), right(nullptr) {}
+  };
+
+  Node* root = new Node(5);
+
+  root->left = new Node(3);
+  root->left->left = new Node(2);
+  root->left->right = new Node(4);
+
+  root->right = new Node(6);
+
+  auto inorderTraversal = [](Node* node, auto&& inorderTraversal) {
+    if (node == nullptr) return;
+
+    inorderTraversal(node->left, inorderTraversal);
+
+    cout << node->data << ' ';
+
+    inorderTraversal(node->right, inorderTraversal);
+  };
+
+  cout << "Inorder of original tree: ";
+  inorderTraversal(root, inorderTraversal);
+  cout << '\n';
+
+  auto invertBinaryTree = [](Node* node, auto&& invertBinaryTree) -> void {
+    if (node == nullptr) return;
+
+    swap(node->left, node->right);
+
+    invertBinaryTree(node->left, invertBinaryTree);
+
+    invertBinaryTree(node->right, invertBinaryTree);
+  };
+
+  invertBinaryTree(root, invertBinaryTree);
+
+  cout << "Inorder of inverted tree: ";
+  inorderTraversal(root, inorderTraversal);
+  cout << '\n';
+}
