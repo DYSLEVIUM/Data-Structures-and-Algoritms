88ea48da0264c90f25f13e03d84b986c0e87c6a8
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-06-17 16:05:20 +0530

    Create diameterOfBinaryTree.cc

---
+++ solvedQuestions/binaryTrees/diameterOfBinaryTree.cc
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    // Function to return the diameter of a Binary Tree.
+    int diameter(Node* root) {
+        // Your code here
+        
+        if (root==NULL) return 0;
+        
+        int dia = 0;
+        auto findHeight = [&dia](Node* node, auto&& findHeight)->int{
+            if(!node) return 0;
+            
+            int l = findHeight(node->left, findHeight);
+            int r = findHeight(node->right, findHeight);
+            
+            dia = max(dia, 1+l+r);
+            
+            return 1+max(l,r);
+        };
+        
+        dia = max(dia, findHeight(root, findHeight));
+        
+        return dia;
+    }
+};
