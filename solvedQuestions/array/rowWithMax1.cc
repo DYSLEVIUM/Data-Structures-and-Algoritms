ca9ad4a65f240f900c93d64a8ba73574d6575bbd
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-07-15 15:08:16 +0530

    Create rowWithMax1.cc

---
+++ solvedQuestions/array/rowWithMax1.cc
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	    // code here
+	    int max1=0;
+	    int i1=-1;
+	
+	    for(int i=0;i<n;++i){
+	        int curr1=0;
+	        for(int j=0;j<m;++j){
+	            if(arr[i][j]==1){
+	                ++curr1;
+	                
+	                if(curr1>max1){
+	                    max1 = curr1;
+	                    i1=i;
+	                }
+	            }
+	        }
+	    }
+	    
+	    return i1;
+	}
+
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector< vector<int> > arr(n,vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin>>arr[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.rowWithMax1s(arr, n, m);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
