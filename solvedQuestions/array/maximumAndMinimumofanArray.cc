499246939fd2598c911e0ae5c5394d40ac1bef78
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-07-19 22:11:26 +0530

    Create maximumAndMinimumofanArray.cc

---
+++ solvedQuestions/array/maximumAndMinimumofanArray.cc
@@ -0,0 +1,103 @@
+#define _USE_MATH_DEFINES
+#pragma GCC optimize("Ofast,fast-math,unroll-loops")
+
+#ifdef DYSLEVIUM
+#include "dyslevium.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<ll, ll> pl;
+typedef std::vector<ll> vl;
+typedef std::vector<pl> vpl;
+typedef std::vector<vl> vvl;
+typedef std::map<ll, ll> mll;
+typedef std::priority_queue<ll> pqd;
+typedef std::priority_queue<ll, vl, std::greater<ll>> pqi;
+
+#define pb push_back
+#define eb emplace_back
+#define MOD (ll)(1e9 + 7)
+
+#define fo(i, n) for (ll i = 0; i < (ll)n; ++i)
+#define Fo(i, k, n) for (ll i = k; k < (ll)n ? i < (ll)n : i > (ll)n; k < (ll)n ? ++i : --i)
+#define all(x) x.begin(), x.end()
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); ++it)
+#define ps(x, y) std::fixed << std::setprecision(y) << x
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+
+std::mt19937_64 rng(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+//  debuging
+#ifdef DYSLEVIUM
+#define deb(x) std::cerr << #x << " = " << x << '\n'
+#else
+#define deb(x)
+#endif
+
+//  initial setup
+inline void setup() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+#ifdef DYSLEVIUM
+  freopen("input.in", "r", stdin);
+  freopen("output.out", "w", stdout);
+  freopen("error.err", "w", stderr);
+#endif
+}
+
+inline void solve();
+
+int main(int argc, char* argv[]) {
+  setup();
+
+  ll t = 1;
+  // std::cin >> t;
+
+  while (t--)
+    solve();
+
+  return 0;
+}
+
+using namespace std;
+
+//  Compile and run: g++ -std=c++17 -g -Wshadow -Wall main.cc -D DYSLEVIUM -o a -Ofast -Wno-unused-result && ./a
+
+inline void solve() {
+  //  tournament method
+  auto getMinMax = [](ll* a, ll lo, ll hi, auto&& getMinMax) -> pl {
+    pl minMax;
+
+    if (lo == hi) minMax.first = a[lo];
+
+    if (lo + 1 == hi) {
+      minMax.first = min(a[hi], a[lo]);
+      minMax.second = max(a[hi], a[lo]);
+
+      return minMax;
+    }
+
+    ll mid = lo + (hi - lo) / 2;
+
+    pl le = getMinMax(a, lo, mid, getMinMax);
+
+    pl ri = getMinMax(a, mid, hi, getMinMax);
+
+    minMax.first = min(le.first, ri.first);
+    minMax.second = max(le.second, ri.second);
+
+    return minMax;
+  };
+
+  ll arr[] = {1000, 11, 445, 1, 330, 3000};
+
+  pl ans = getMinMax(arr, 0, 5, getMinMax);
+
+  cout << ans.first << ' ' << ans.second;
+}
