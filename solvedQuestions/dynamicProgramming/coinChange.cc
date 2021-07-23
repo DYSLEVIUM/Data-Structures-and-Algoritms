045b7bf820a4b6c5bcb95b4cfec72a4de8184aaf
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-07-23 13:41:56 +0530

    Update coinChange.cc

--- solvedQuestions/dynamicProgramming/coinChange.cc
+++ solvedQuestions/dynamicProgramming/coinChange.cc
@@ -1,102 +1,48 @@
-#define _USE_MATH_DEFINES
-#pragma GCC optimize("Ofast,fast-math")
-
-#include <bits/stdc++.h>
-
-typedef long long ll;
-typedef long double ld;
-typedef std::pair<int, int> pii;
-typedef std::pair<long, long> pl;
-typedef std::vector<int> vi;
-typedef std::vector<ll> vl;
-typedef std::vector<pii> vpii;
-typedef std::vector<pl> vpl;
-typedef std::vector<vi> vvi;
-typedef std::vector<vl> vvl;
-typedef std::map<int, int> mii;
-typedef std::priority_queue<int> pqd;
-typedef std::priority_queue<int, vi, std::greater<int>> pqi;
-
-#define eb emplace_back
-#define F first
-#define S second
-#define MOD (long long)1e9 + 7
-#define PI 3.14159265358979323846
-#define INF __builtin_inff()
-
-#define fo(i, n) for (ll i = 0; i < n; ++i)
-#define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
-#define allC(x) x.begin(), x.end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define deb(x) std::cout << '\n' \
-                         << #x << " = " << x << '\n'
-#define sortall(x) sort(x.begin(), x.end())
-#define tr(it, a) for (auto it = a.begin(); it != a.end(); ++it)
-#define ps(x, y) std::fixed << std::setprecision(y) << x
-#define setbits(x) __builtin_popcountll(x)
-#define zerobits(x) __builtin_ctzll(x)
-#define mk(arr, n, type) type* arr = new type[n]
-
-std::mt19937_64 rng(std::chrono::high_resolution_clock::now().time_since_epoch().count());
-
-inline void setup() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
-
-#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
-    freopen("input.txt", "r", stdin);
-#else
-#ifndef ONLINE_JUDGE  // runs automatically for supported online judges
-    freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-#endif
-#endif
-}
-
-inline void solve();
-
-int main(int argc, char* argv[]) {
-    setup();
-
-    ll t = 1;
-    // std::cin >> t;
-
-    while (t--)
-        solve();
-
-    return 0;
-}
-
+// { Driver Code Starts
+#include<bits/stdc++.h>
 using namespace std;
-//  Compile and run: g++ -g -Wshadow -Wall practice.cpp -o a.exe -Ofast -Wno-unused-result && ./a.exe
 
-//  https://www.hackerrank.com/challenges/coin-change/problem
-inline void solve() {
-    ll n, m;
-    cin >> n >> m;
-
-    mk(c, m, ll);
-    fo(i, m) cin >> c[i];
-
-    mk(dp, n + 1, ll*);
-    fo(i, n + 1) dp[i] = new ll[m + 1];
-
-    fo(i, n + 1) {
-        fo(j, m + 1) {
-            if (i == 0) dp[i][j] = 1;
-            if (j == 0)
-                dp[i][j] = 0;
+ // } Driver Code Ends
+class Solution
+{
+  public:
+    long long int count( int S[], int m, int n )
+    {
+        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
+    
+        for(int i = 0; i < n + 1; ++i){
+            for(int j = 0; j < m + 1; ++j){
+                if(i == 0 || j == 0){
+                    if(j == 0) dp[i][j] = 0;
+                    if(i == 0) dp[i][j] = 1;
+                }else{
+                    dp[i][j] += dp[i][j-1];
+                    
+                    if(i - S[j-1] >= 0) dp[i][j] += dp[i - S[j - 1]][j];
+                }
+            }
         }
+        
+        return dp[n][m];
     }
+};
 
-    Fo(i, 1, n + 1) {
-        Fo(j, 1, m + 1) {
-            dp[i][j] = dp[i][j - 1];
-
-            if (i - c[j - 1] >= 0) dp[i][j] += dp[i - c[j - 1]][j];
-        }
-    }
-
-    cout << dp[n][m];
-}
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+	while(t--)
+	{
+		int n,m;
+		cin>>n>>m;
+		int arr[m];
+		for(int i=0;i<m;i++)
+		    cin>>arr[i];
+	    Solution ob;
+		cout<<ob.count(arr,m,n)<<endl;
+	}
+    
+    
+    return 0;
+}  // } Driver Code Ends
