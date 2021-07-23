7f50ec550955953ca9516c08b709aa0b85a1a20e
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-07-23 14:09:25 +0530

    Update coinChange.cc

--- solvedQuestions/dynamicProgramming/coinChange.cc
+++ solvedQuestions/dynamicProgramming/coinChange.cc
@@ -8,22 +8,41 @@ class Solution
   public:
     long long int count( int S[], int m, int n )
     {
-        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
+//	Time: O(nm) 
+//	Space: O(nm) 
+//         vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
     
-        for(int i = 0; i < n + 1; ++i){
-            for(int j = 0; j < m + 1; ++j){
-                if(i == 0 || j == 0){
-                    if(j == 0) dp[i][j] = 0;
-                    if(i == 0) dp[i][j] = 1;
-                }else{
-                    dp[i][j] += dp[i][j-1];
+//         for(int i = 0; i < n + 1; ++i){
+//             for(int j = 0; j < m + 1; ++j){
+//                 if(i == 0 || j == 0){
+//                     if(j == 0) dp[i][j] = 0;
+//                     if(i == 0) dp[i][j] = 1;
+//                 }else{
+//                     dp[i][j] += dp[i][j-1];
                     
-                    if(i - S[j-1] >= 0) dp[i][j] += dp[i - S[j - 1]][j];
+//                     if(i - S[j-1] >= 0) dp[i][j] += dp[i - S[j - 1]][j];
+//                 }
+//             }
+//         }
+        
+//         return dp[n][m];
+	
+//	Time: O(nm) 
+//	Space: O(n) 
+	    
+	vector<long long> dp(n + 1, 0);
+        dp[0] = 1;
+        
+	//	here, we first iterate over the available coins for a current value (if we do it the other way, we also end up addng the permutation, which is incorrect)
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n + 1; ++j){
+                if(j - S[i] >= 0){
+                    dp[j] += dp[j - S[i]];
                 }
             }
         }
         
-        return dp[n][m];
+        return dp[n];
     }
 };
 
