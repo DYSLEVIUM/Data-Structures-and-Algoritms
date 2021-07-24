87742a2e2ddcbb5ec6ccb49552ca15c194f3dc67
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-07-24 19:32:11 +0530

    Update LCSof3Strings.cc

--- solvedQuestions/dynamicProgramming/LCSof3Strings.cc
+++ solvedQuestions/dynamicProgramming/LCSof3Strings.cc
@@ -1,90 +1,38 @@
-#define _USE_MATH_DEFINES
-#pragma GCC optimize("Ofast,fast-math")
-
-#include <bits/stdc++.h>
-
-typedef long long ll;
-typedef long double ld;
-typedef std::pair<int, int> pii;
-typedef std::pair<long, long> pl;
-typedef std::vector<int> vi;
-typedef std::vector<ll> vl;
-typedef std::vector<pii> vpii;
-typedef std::vector<pl> vpl;
-typedef std::vector<vi> vvi;
-typedef std::vector<vl> vvl;
-typedef std::map<int, int> mii;
-typedef std::priority_queue<int> pqd;
-typedef std::priority_queue<int, vi, std::greater<int>> pqi;
-
-#define eb emplace_back
-#define F first
-#define S second
-#define MOD (ll)(1e9 + 7)
-#define PI 3.14159265358979323846
-#define INF __builtin_inff()
-
-#define fo(i, n) for (ll i = 0; i < n; ++i)
-#define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
-#define allC(x) x.begin(), x.end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define deb(x) std::cout << '\n' \
-                         << #x << " = " << x << '\n'
-#define sortall(x) sort(x.begin(), x.end())
-#define tr(it, a) for (auto it = a.begin(); it != a.end(); ++it)
-#define ps(x, y) std::fixed << std::setprecision(y) << x
-#define setbits(x) __builtin_popcountll(x)
-#define zerobits(x) __builtin_ctzll(x)
-#define mk(arr, n, type) type* arr = new type[n]
-
-std::mt19937_64 rng(std::chrono::high_resolution_clock::now().time_since_epoch().count());
-
-inline void setup() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
-
-#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
-    freopen("input.txt", "r", stdin);
-#else
-#ifndef ONLINE_JUDGE  // runs automatically for supported online judges
-    freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-#endif
-#endif
-}
-
-inline void solve();
-
-int main(int argc, char* argv[]) {
-    setup();
-
-    ll t = 1;
-    // std::cin >> t;
-
-    while (t--)
-        solve();
-
-    return 0;
-}
-
+// { Driver Code Starts
+#include<bits/stdc++.h>
 using namespace std;
-//  Compile and run: g++ -g -Wshadow -Wall practice.cpp -o a.exe -Ofast -Wno-unused-result && ./a.exe
 
-inline void solve() {
-    auto lcs = [](string s1, string s2, string s3) -> ll {
-        ll*** dp = new ll**[s1.length() + 1];
+int LCSof3 (string A, string B, string C, int n1, int n2, int n3);
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; cin >> n1 >> n2 >> n3;
+        string A, B, C; cin >> A >> B >> C;
+        cout << LCSof3 (A, B, C, n1, n2, n3) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
 
-        for (ll i{}; i <= s1.length(); ++i) {
-            dp[i] = new ll*[s2.length() + 1];
 
-            for (ll j{}; j <= s2.length() + 1; ++j)
-                dp[i][j] = new ll[s3.length() + 1];
+int LCSof3 (string s1, string s2, string s3, int n1, int n2, int n3)
+{
+    // your code here
+    typedef long long ll;
+    ll*** dp = new ll**[n1 + 1];
+
+        for (ll i{}; i < n1 + 1; ++i) {
+            dp[i] = new ll*[n2 + 1];
+
+            for (ll j{}; j < n2 + 1; ++j)
+                dp[i][j] = new ll[n3 + 1];
         }
 
-        for (ll i{}; i <= s1.length(); ++i) {
-            for (ll j{}; j <= s2.length(); ++j) {
-                for (ll k{}; k <= s3.length(); ++k) {
+        for (ll i{}; i < n1 + 1; ++i) {
+            for (ll j{}; j < n2 + 1; ++j) {
+                for (ll k{}; k < n3 + 1; ++k) {
                     if (i == 0 || j == 0 || k == 0)
                         dp[i][j][k] = 0;
                     else if (s1[i - 1] == s2[j - 1] && s3[k - 1] == s1[i - 1])
@@ -95,13 +43,5 @@ inline void solve() {
             }
         }
 
-        return dp[s1.length()][s2.length()][s3.length()];
-    };
-
-    string s1 = "abcd";
-    string s2 = "efgh";
-    string s3 = "ijkl";
-
-    // cout << lcs(s1, s2);
-    deb(lcs(s1, s2, s3));
+    return dp[n1][n2][n3];
 }
