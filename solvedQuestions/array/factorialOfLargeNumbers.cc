15fab87c010db5aa3ea9fa4947d19c02f8b89512
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-07-26 16:18:51 +0545

    Update factorialOfLargeNumbers.cc

--- solvedQuestions/array/factorialOfLargeNumbers.cc
+++ solvedQuestions/array/factorialOfLargeNumbers.cc
@@ -1,74 +1,60 @@
-#define _USE_MATH_DEFINES
-#pragma GCC optimize("Ofast,fast-math")
-
+// { Driver Code Starts
+// Initial Template for C++
 #include <bits/stdc++.h>
-
-#define deb(x) cout << '\n' \
-                    << #x << " = " << x << '\n'
-
-inline void setup() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
-
-#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
-    freopen("input.txt", "r", stdin);
-#else
-#ifndef ONLINE_JUDGE  // runs automatically for supported online judges
-    freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-#endif
-#endif
-}
-
-inline void solve();
-
-int main(int argc, char* argv[]) {
-    setup();
-
-    long long t = 1;
-    std::cin >> t;
-
-    while (t--)
-        solve();
-
-    return 0;
-}
-
-//Compile: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result
-//Build: g++ -g -Wshadow -Wall main.cc -o a.exe -D_GLIBCXX_DEBUG
-//Compile and run: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result && ./a.exe
-
 using namespace std;
 
-//	https://practice.geeksforgeeks.org/problems/factorials-of-large-numbers/0#
+ // } Driver Code Ends
+//User function template for C++
 
-inline void solve() {
-    int n;
-    cin >> n;
+class Solution {
+public:
+    vector<int> factorial(int N){
+        // code here
+        const int MXN = 1e5;
+        vector<int> digits(MXN);
+        digits[0] = 1;
+        
+        int resSize = 1;
 
-    const int N = 1e5;
-    int* res = new int[N]{1};
-
-    int resSize = 1;
-
-    for (int i = 2; i <= n; ++i) {
-        int carry = 0;
-
-        for (int j = 0; j < resSize; ++j) {
-            int prod = res[j] * i + carry;
-
-            res[j] = prod % 10;
-            carry = prod / 10;
+        for (int i = 2; i <= N; ++i) {
+            int carry = 0;
+    
+            for (int j = 0; j < resSize; ++j) {
+                int prod = digits[j] * i + carry;
+    
+                digits[j] = prod % 10;
+                carry = prod / 10;
+            }
+    
+            while (carry) {
+                digits[resSize++] = carry % 10;
+                carry /= 10;
+            }
         }
-
-        while (carry) {
-            res[resSize] = carry % 10;
-            carry /= 10;
-            ++resSize;
-        }
+    
+        
+        vector<int> ans(digits.begin(), digits.begin()+resSize);
+        
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
     }
+};
 
-    for (int i = resSize - 1; i >= 0; --i) cout << res[i];
-    cout << '\n';
-}
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        vector<int> result = ob.factorial(N);
+        for (int i = 0; i < result.size(); ++i){
+            cout<< result[i];
+        }
+        cout << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
