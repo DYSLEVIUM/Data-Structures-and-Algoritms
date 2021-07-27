85a32f949fb5262a75aca5d9ef20c0bc73cb11b7
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-07-27 14:26:58 +0545

    Create middleofThree.cc

---
+++ solvedQuestions/searchingAndSorting/middleofThree.cc
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int middle(int A, int B, int C){
+        //code here
+        
+        vector<int> a{A,B,C};
+        sort(a.begin(),a.end());
+        
+        return a[1];
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A,B,C;
+        cin>>A>>B>>C;
+        Solution ob;
+        cout<<ob.middle(A,B,C) <<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
