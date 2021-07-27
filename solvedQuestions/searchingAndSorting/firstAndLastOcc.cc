d62940f3ce05ae899013da43cfa7569b00c401c7
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-07-27 13:02:56 +0545

    Update firstAndLastOcc.cc

--- solvedQuestions/searchingAndSorting/firstAndLastOcc.cc
+++ solvedQuestions/searchingAndSorting/firstAndLastOcc.cc
@@ -1,98 +1,42 @@
-#pragma GCC optimize("Ofast,fast-math")
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> find(int a[], int n , int x );
 
-#include <bits/stdc++.h>
-
-#define deb(x) cout << '\n' \
-                    << #x << " = " << x << '\n'
-
-inline void setup() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
-
-#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
-    freopen("input.txt", "r", stdin);
-#else
-#ifndef ONLINE_JUDGE  // runs automatically for supported online judges
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-#endif
-}
-
-inline void solve();
-
-int main(int argc, char* argv[]) {
-    setup();
-
-    long long t = 1;
-    std::cin >> t;
-
-    while (t--) solve();
-
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int arr[n],i;
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        vector<int> ans;
+        ans=find(arr,n,x);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
     return 0;
 }
 
-//Compile: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result
-//Build: g++ -g -Wshadow -Wall main.cc -o a.exe -D_GLIBCXX_DEBUG
-//Compile and run: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result && a.exe
 
-using namespace std;
-//  https://practice.geeksforgeeks.org/problems/first-and-last-occurrences-of-x/0#
+// } Driver Code Ends
 
-int bsr(int* a, int x, int low, int high) {
-    int idx = -1;
 
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-
-        if (a[mid] == x) {
-            idx = mid;
-        }
-        if (a[mid] <= x) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
-        }
+vector<int> find(int arr[], int n , int x )
+{
+    // code here
+    vector<int> res(2);
+    
+    if(!binary_search(arr,arr+n,x)){
+        res[0]=-1;    
+        res[1]=-1;    
+    }else{
+        res[0] = lower_bound(arr,arr+n,x)-arr;
+        res[1] = upper_bound(arr,arr+n,x)-arr-1;
     }
-
-    return idx;
-}
-
-int bsl(int* a, int x, int low, int high) {
-    int idx = -1;
-
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-
-        if (a[mid] == x) {
-            idx = mid;
-        }
-        if (a[mid] >= x) {
-            high = mid - 1;
-        } else {
-            low = mid + 1;
-        }
-    }
-
-    return idx;
-}
-
-inline void solve() {
-    int n, x;
-    cin >> n >> x;
-
-    int* a = new int[n];
-
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-    }
-
-    int low = 0;
-    int high = n - 1;
-
-    if (bsl(a, x, low, high) != -1) {
-        cout << bsl(a, x, low, high) << ' ' << bsr(a, x, low, high) << '\n';
-    } else
-        cout << -1 << '\n';
+    
+    return res;
 }
