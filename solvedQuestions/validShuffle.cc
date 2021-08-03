19e9c9781ce0450633d998837b0ea739591700f9
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-08-03 16:02:34 +0545

    Create validShuffle.cc

---
+++ solvedQuestions/validShuffle.cc
@@ -0,0 +1,100 @@
+#define _USE_MATH_DEFINES
+#pragma GCC optimize("Ofast,fast-math,unroll-loops")
+
+#ifdef DYSLEVIUM
+#include "dyslevium.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<ll, ll> pl;
+typedef std::vector<ll> vl;
+typedef std::vector<pl> vpl;
+typedef std::vector<vl> vvl;
+typedef std::map<ll, ll> mll;
+typedef std::priority_queue<ll> pqd;
+typedef std::priority_queue<ll, vl, std::greater<ll>> pqi;
+
+#define pb push_back
+#define eb emplace_back
+#define MOD (ll)(1e9 + 7)
+
+#define fo(i, n) for (ll i = 0; i < (ll)n; ++i)
+#define Fo(i, k, n) for (ll i = k; k < (ll)n ? i < (ll)n : i > (ll)n; k < (ll)n ? ++i : --i)
+#define all(x) x.begin(), x.end()
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); ++it)
+#define ps(x, y) std::fixed << std::setprecision(y) << x
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+
+std::mt19937_64 rng(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+//  debuging
+#ifdef DYSLEVIUM
+#define deb(x) std::cerr << #x << " = " << x << '\n'
+#else
+#define deb(x)
+#endif
+
+//  initial setup
+inline void setup() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  std::cerr.tie(nullptr);
+
+#ifdef DYSLEVIUM
+  freopen("input.in", "r", stdin);
+  freopen("output.out", "w", stdout);
+  freopen("error.err", "w", stderr);
+#endif
+}
+
+inline void solve();
+
+int main(int argc, char* argv[]) {
+  setup();
+
+  ll t = 1;
+  // std::cin >> t;
+
+  while (t--)
+    solve();
+
+  return 0;
+}
+
+using namespace std;
+
+//  Compile and run: g++ -std=c++17 -g -Wshadow -Wall main.cc -D DYSLEVIUM -o a -Ofast -Wno-unused-result && ./a
+
+inline void solve() {
+  string x = "1XY2", y = "Y1X2", res = "YY2211ZX";
+
+  sort(all(x));
+  sort(all(y));
+  sort(all(res));
+
+  ll i = 0, j = 0, k = 0;
+
+  if (x.size() + y.size() != res.size()) {
+    cout << "NO\n";
+    return;
+  }
+
+  while (i < x.size() || j < y.size()) {
+    if (i < x.size() && x[i] == res[k]) {
+      ++i;
+    } else if (j < y.size() && y[j] == res[k]) {
+      ++j;
+    } else {
+      cout << "NO\n";
+      return;
+    }
+    ++k;
+  }
+
+  cout << "YES\n";
+}
