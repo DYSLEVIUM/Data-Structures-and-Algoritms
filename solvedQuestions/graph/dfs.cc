c33a8f5a612b5a220271976195d51469609b7755
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-08-10 13:51:50 +0545

    Create dfs.cc

---
+++ solvedQuestions/graph/dfs.cc
@@ -0,0 +1,106 @@
+#define _USE_MATH_DEFINES
+#pragma GCC optimize("Ofast,fast-math,unroll-loops")
+
+#ifdef DYSLEVIUM
+#include "dyslevium.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<ll, ll> pl;
+typedef std::vector<ll> vl;
+typedef std::vector<pl> vpl;
+typedef std::vector<vl> vvl;
+typedef std::map<ll, ll> mll;
+typedef std::priority_queue<ll> pqd;
+typedef std::priority_queue<ll, vl, std::greater<ll>> pqi;
+
+#define pb push_back
+#define eb emplace_back
+#define MOD (ll)(1e9 + 7)
+
+#define fo(i, n) for (ll i = 0; i < (ll)n; ++i)
+#define Fo(i, k, n) for (ll i = k; k < (ll)n ? i < (ll)n : i > (ll)n; k < (ll)n ? ++i : --i)
+#define all(x) x.begin(), x.end()
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); ++it)
+#define ps(x, y) std::fixed << std::setprecision(y) << x
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+
+std::mt19937_64 rng(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+//  debuging
+#ifdef DYSLEVIUM
+#define deb(x) std::cerr << #x << " = " << x << '\n'
+#else
+#define deb(x)
+#endif
+
+//  initial setup
+inline void setup() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  std::cerr.tie(nullptr);
+
+#ifdef DYSLEVIUM
+  freopen("input.in", "r", stdin);
+  freopen("output.out", "w", stdout);
+  freopen("error.err", "w", stderr);
+#endif
+}
+
+inline void solve();
+
+int main(int argc, char* argv[]) {
+  setup();
+
+  ll t = 1;
+  // std::cin >> t;
+
+  while (t--)
+    solve();
+
+  return 0;
+}
+
+using namespace std;
+
+//  Compile and run: g++ -std=c++17 -g -Wshadow -Wall main.cc -D DYSLEVIUM -o a -Ofast -Wno-unused-result && ./a
+
+inline void solve() {
+  vvl adjList;  //  graph as adjency list
+
+  //	building the graph
+  adjList.emplace_back(vl{1, 4});
+  adjList.emplace_back(vl{0, 2, 4});
+  adjList.emplace_back(vl{1, 3});
+  adjList.emplace_back(vl{2, 5, 4});
+  adjList.emplace_back(vl{0, 1, 3});
+  adjList.emplace_back(vl{3});
+
+  auto printGraph = [](const vvl& adjList) {
+    fo(i, adjList.size()) {
+      cout << i + 1 << " -> ";
+      fo(j, adjList[i].size()) cout << adjList[i][j] << ' ';
+      cout << '\n';
+    }
+  };
+
+  vl dfsArr(0);
+  vl visited(adjList.size(), false);
+  auto dfs = [&dfsArr, &visited](const vvl& gr, const ll& idx, const auto& dfs) {
+    if (visited[idx]) return;
+
+    visited[idx] = true;
+    dfsArr.eb(idx);
+
+    fo(i, gr[idx].size()) dfs(gr, gr[idx][i], dfs);
+  };
+
+  dfs(adjList, 0, dfs);
+
+  tr(it, dfsArr) cout << *it << ' ';
+}
