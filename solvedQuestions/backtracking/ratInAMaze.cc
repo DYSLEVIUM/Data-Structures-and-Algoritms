61a452e84ac90cf8e5de6e7eb4739852eac4ff7b
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-08-10 14:11:28 +0545

    Update ratInAMaze.cc

--- solvedQuestions/backtracking/ratInAMaze.cc
+++ solvedQuestions/backtracking/ratInAMaze.cc
@@ -10,40 +10,41 @@ using namespace std;
 
 class Solution{
     public:
-    vector<string> ans;
-    
-    void findPath(vector<vector<int>> &m, int n, vector<vector<bool>>& visited, int row=0, int col=0, string currAns=""){
-        if(row>=n || col>=n || row<0 || col<0 || m[row][col]==0 || visited[row][col]==true) return;
-        
-        if(row==n-1 && col==n-1) {
-            ans.push_back(currAns);
-            return;
-        }
-        
-        int x[] = {1,0,-1,0};
-        int y[] = {0,1,0,-1};
-        
-        visited[row][col] = true;
-        
-        for(int i=0;i<4;++i){
-            string tempS = currAns;
-            
-            if(i==0) tempS+='D';
-            else if(i==1) tempS+='R';
-            else if(i==2) tempS+='U';
-            else tempS+='L';                
-            
-            findPath(m, n, visited, row+x[i], col+y[i], tempS);
-        }
-        
-        visited[row][col] = false;
-    }
     
     vector<string> findPath(vector<vector<int>> &m, int n) {
         // Your code goes here
-        vector<vector<bool>> visited(n+1, vector<bool>(n+1, false));
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        vector<string> ans(0);
         
-        findPath(m, n, visited);
+        auto dfs = [&ans, &m, &n, &visited](const auto& dfs, int row=0, int col=0, string currAns=""){
+            if(row>=n || col>=n || row<0 || col<0 || m[row][col]==0 || visited[row][col]==true) return;
+        
+            if(row==n-1 && col==n-1) {
+                ans.push_back(currAns);
+                return;
+            }
+            
+            int x[] = {1,0,-1,0};
+            int y[] = {0,1,0,-1};
+            
+            visited[row][col] = true;
+            
+            for(int i=0;i<4;++i){
+                string tempS = currAns;
+                
+                if(i==0) tempS+='D';
+                else if(i==1) tempS+='R';
+                else if(i==2) tempS+='U';
+                else tempS+='L';                
+                
+                dfs(dfs, row+x[i], col+y[i], tempS);
+            }
+            
+            //  backtracking
+            visited[row][col] = false;
+        };
+        
+        dfs(dfs);
         
         sort(ans.begin(), ans.end());
         
