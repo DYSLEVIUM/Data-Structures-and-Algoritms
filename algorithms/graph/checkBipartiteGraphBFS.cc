64e5e94c8cc9c85d34f6e89915ff46c8d58aac5a
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-08-12 14:40:15 +0545

    Create checkBipartiteGraphBFS.cc

---
+++ algorithms/graph/checkBipartiteGraphBFS.cc
@@ -0,0 +1,171 @@
+#define _USE_MATH_DEFINES
+#pragma GCC optimize("Ofast,fast-math,unroll-loops")
+
+#ifdef DYSLEVIUM
+#include "dyslevium.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<ll, ll> pl;
+typedef std::vector<ll> vl;
+typedef std::vector<pl> vpl;
+typedef std::vector<vl> vvl;
+typedef std::map<ll, ll> mll;
+typedef std::priority_queue<ll> pqd;
+typedef std::priority_queue<ll, vl, std::greater<ll>> pqi;
+
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+#define MOD (ll)(1e9 + 7)
+
+std::mt19937_64 RNG(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+//  macro functions
+#define fo(i, n) for (ll i = 0; i < (ll)n; ++i)
+#define Fo(i, k, n) for (ll i = k; k < (ll)n ? i < (ll)n : i > (ll)n; k < (ll)n ? ++i : --i)
+#define all(x) x.begin(), x.end()
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); ++it)
+#define ps(x, y) std::fixed << std::setprecision(y) << x
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define modAdd(a, b) ((((a % MOD) + (b % MOD)) % MOD) + MOD) % MOD
+#define modSub(a, b) ((((a % MOD) - (b % MOD)) % MOD) + MOD) % MOD
+#define modMul(a, b) ((((a % MOD) * (b % MOD)) % MOD) + MOD) % MOD
+
+//  template functions
+template <typename T>
+inline T gcd(const T& a, const T& b) {
+  if (b) return gcd(b, a % b);
+  return a;
+}
+template <typename T>
+inline T binPowIter(T x, T n) {
+  T res = 1;
+  while (n) {
+    if (n & 1) res = modMul(res, x);
+    x = modMul(x, x);
+    n >>= 1;
+  }
+  return res % MOD;
+}
+template <typename T>
+inline T modInverse(const T& a) { return binPowIter(a, MOD - 2); }
+template <typename T>
+inline T modDiv(const T& a, const T& b) { return (modMul(a, modInverse(b)) + MOD) % MOD; }
+
+//  debuging
+#ifdef DYSLEVIUM
+#define deb(x) std::cerr << #x << " = " << x << '\n'
+#else
+#define deb(x)
+#endif
+
+//  initial setup
+inline void setup() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  std::cerr.tie(nullptr);
+
+#ifdef DYSLEVIUM
+  freopen("input.in", "r", stdin);
+  freopen("output.out", "w", stdout);
+  freopen("error.err", "w", stderr);
+#endif
+}
+
+inline void solve();
+
+int main(int argc, char* argv[]) {
+  setup();
+
+  auto startTime = std::chrono::high_resolution_clock::now();
+
+  ll t = 1;
+  // std::cin >> t;
+
+  while (t--) solve();
+
+  auto endTime = std::chrono::high_resolution_clock::now();
+
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+
+#ifdef DYSLEVIUM
+  std::cerr << "Time: " << duration.count();
+#endif
+
+  return 0;
+}
+
+using namespace std;
+
+//  Compile and run: g++ -std=c++17 -g -Wshadow -Wall main.cc -D DYSLEVIUM -o a -Ofast -Wno-unused-result && ./a
+
+inline void solve() {
+  vvl adjList1;
+
+  adjList1.eb(vl{1});
+  adjList1.eb(vl{0, 2});
+  adjList1.eb(vl{1, 3});
+  adjList1.eb(vl{2, 4});
+  adjList1.eb(vl{3, 5, 7});
+  adjList1.eb(vl{4, 6});
+  adjList1.eb(vl{5});
+  adjList1.eb(vl{4});
+
+  vvl adjList2;
+
+  adjList2.eb(vl{1});
+  adjList2.eb(vl{0, 2, 7});
+  adjList2.eb(vl{1, 3});
+  adjList2.eb(vl{2, 4});
+  adjList2.eb(vl{3, 5, 7});
+  adjList2.eb(vl{4, 6});
+  adjList2.eb(vl{5});
+  adjList2.eb(vl{1, 4});
+
+  auto isBipartite = [](const vvl& gr) {
+    vl color(gr.size(), -1);  //  initially no color
+
+    auto bfsCheck = [&gr, &color](const ll& node) {
+      queue<ll> qu;
+
+      //  initial node with color 0
+      qu.push(node);
+      color[node] = 0;
+
+      while (!qu.empty()) {
+        ll nd = qu.front();
+        qu.pop();
+
+        for (auto neighbour : gr[nd]) {
+          if (color[neighbour] == -1) {
+            color[neighbour] = 1 - color[nd];
+            qu.push(neighbour);
+          } else {
+            if (color[neighbour] == color[nd]) return false;
+          }
+        }
+      }
+
+      return true;
+    };
+
+    //  done so that multiple components are covered
+    fo(i, gr.size()) {
+      if (color[i] == -1) {
+        if (!bfsCheck(i)) return false;
+      }
+    }
+
+    return true;
+  };
+
+  cout << isBipartite(adjList1) << '\n';
+  cout << isBipartite(adjList2) << '\n';
+}
