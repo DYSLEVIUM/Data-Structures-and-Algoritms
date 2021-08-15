eaaa2652c050ec349cf544b30109eaa5005da92d
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-08-15 14:39:14 +0545

    Update 0026-removeDuplicatesfromSortedArray.cc

--- leetcodeSolutions/0026-removeDuplicatesfromSortedArray.cc
+++ leetcodeSolutions/0026-removeDuplicatesfromSortedArray.cc
@@ -1,33 +1,40 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-      set<int> pos;
+//       set<int> pos;
       
-      for(int i=0;i<nums.size();++i){
-        int j = i+1;
-        while(j<nums.size() && nums[i]==nums[j]){
-          pos.insert(j);
-          nums[j++] = INT_MAX;
+//       for(int i=0;i<nums.size();++i){
+//         int j = i+1;
+//         while(j<nums.size() && nums[i]==nums[j]){
+//           pos.insert(j);
+//           nums[j++] = INT_MAX;
+//         }
+
+//         i=j-1;
+//       }
+      
+//       int k = nums.size() - pos.size();
+      
+//       if(!pos.empty()){
+//         for(int i=0;i<nums.size();++i){
+//           int fr = *pos.begin();
+
+//           if(nums[i]!=INT_MAX && fr<i){
+//             swap(nums[i], nums[fr]);
+
+//             pos.erase(fr);
+//             pos.insert(i);
+//           }
+//         }  
+//       }
+      
+//       return k;
+      
+        int cnt = 0;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] == nums[i-1]) cnt++;
+            else nums[i-cnt] = nums[i];
         }
-
-        i=j-1;
-      }
-      
-      int k = nums.size() - pos.size();
-      
-      if(!pos.empty()){
-        for(int i=0;i<nums.size();++i){
-          int fr = *pos.begin();
-
-          if(nums[i]!=INT_MAX && fr<i){
-            swap(nums[i], nums[fr]);
-
-            pos.erase(fr);
-            pos.insert(i);
-          }
-        }  
-      }
-      
-      return k;
+        return nums.size()-cnt;
     }
 };
