ae380b2a5244a650fb1c5ccc6512a7bb645750aa
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-08-25 14:28:08 +0545

    Update trappingRainwater.cc

--- solvedQuestions/array/trappingRainwater.cc
+++ solvedQuestions/array/trappingRainwater.cc
@@ -1,104 +1,61 @@
-#define _USE_MATH_DEFINES
-#pragma GCC optimize("Ofast,fast-math")
-
-#include <bits/stdc++.h>
-
-#define deb(x) cout << '\n' \
-                    << #x << " = " << x << '\n'
-
-inline void setup() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
-
-#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
-    freopen("input.txt", "r", stdin);
-#else
-#ifndef ONLINE_JUDGE  // runs automatically for supported online judges
-    freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-#endif
-#endif
-}
-
-inline void solve();
-
-int main(int argc, char* argv[]) {
-    setup();
-
-    long long t = 1;
-    std::cin >> t;
-
-    while (t--)
-        solve();
-
-    return 0;
-}
-
-//Compile: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result
-//Build: g++ -g -Wshadow -Wall main.cc -o a.exe -D_GLIBCXX_DEBUG
-//Compile and run: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result && ./a.exe
+// { Driver Code Starts
+#include<bits/stdc++.h>
 
 using namespace std;
 
-//	https://practice.geeksforgeeks.org/problems/trapping-rain-water-1587115621/1
 
-class Solution {
+ // } Driver Code Ends
+
+class Solution{
+
     // Function to find the trapped water between the blocks.
-   public:
-    int trappingWater(int* arr, int n) {
+    public:
+    int trappingWater(int arr[], int n){
         // Code here
-
-        int mxL = 0;
-        int mxLI = 0;
-
-        int* mL = new int[n];
-
-        for (int i = 0; i < n; ++i) {
-            if (mxL < arr[i]) {
-                mxL = arr[i];
-                mxLI = i;
-            }
-            mL[i] = mxLI;
-        }
-
-        int mxR = 0;
-        int mxRI = n - 1;
-
-        int* mR = new int[n];
-
-        for (int i = n - 1; i >= 0; --i) {
-            if (mxR < arr[i]) {
-                mxR = arr[i];
-                mxRI = i;
-            }
-            mR[i] = mxRI;
-        }
-
-        int sum = 0;
-
-        for (int i = 0; i < n; ++i) {
-            sum += (min(arr[mL[i]], arr[mR[i]]) - arr[i]);
-        }
-
-        delete[] mL;
-        delete[] mR;
-        return sum;
+        
+        //  max from left
+        vector<int> prefix(n);
+        prefix[0]=arr[0];
+        for(int i=0;i<n;++i) prefix[i] = max(arr[i], prefix[i-1]);
+        
+        //  max from right
+        vector<int> suffix(n);
+        suffix[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;--i) suffix[i] = max(arr[i], suffix[i+1]);
+        
+        int ans=0;
+    
+        for(int i=0;i<n;++i) ans+=min(prefix[i],suffix[i])-arr[i];
+        
+        return ans;
     }
 };
 
-inline void solve() {
-    int n;
+// { Driver Code Starts.
 
-    //size of array
-    cin >> n;
-
-    int* a = new int[n];
-
-    //adding elements to the array
-    for (int i = 0; i < n; i++) cin >> a[i];
-
-    Solution obj;
-    //calling trappingWater() function
-    cout << obj.trappingWater(a, n) << '\n';
-}
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        
+        int a[n];
+        
+        //adding elements to the array
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        Solution obj;
+        //calling trappingWater() function
+        cout << obj.trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}  // } Driver Code Ends
