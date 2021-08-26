1a374450efe98eae3563d856e8f8e68c014e6e15
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-08-26 12:32:12 +0545

    Update longestConsecutiveSubsequence.cc

--- solvedQuestions/array/longestConsecutiveSubsequence.cc
+++ solvedQuestions/array/longestConsecutiveSubsequence.cc
@@ -1,70 +1,55 @@
-#define _USE_MATH_DEFINES
-#pragma GCC optimize("Ofast,fast-math")
-
+// { Driver Code Starts
 #include <bits/stdc++.h>
-
-#define deb(x) cout << '\n' \
-                    << #x << " = " << x << '\n'
-
-inline void setup() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
-
-#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
-    freopen("input.txt", "r", stdin);
-#else
-#ifndef ONLINE_JUDGE  // runs automatically for supported online judges
-    freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-#endif
-#endif
-}
-
-inline void solve();
-
-int main(int argc, char* argv[]) {
-    setup();
-
-    long long t = 1;
-    // std::cin >> t;
-
-    while (t--)
-        solve();
-
-    return 0;
-}
-
-//Compile: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result
-//Build: g++ -g -Wshadow -Wall main.cc -o a.exe -D_GLIBCXX_DEBUG
-//Compile and run: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result && ./a.exe
-
 using namespace std;
 
-//	https://practice.geeksforgeeks.org/problems/longest-consecutive-subsequence2449/1
 
-int findLongestConseqSubseq(int* arr, int N) {
-    sort(arr, arr + N);
-
-    int res = 1;
-
-    int cnt = 1;
-    for (int i = 0; i < N - 1; ++i) {
-        if (arr[i + 1] - arr[i] == 1)
-            ++cnt;
-        else if (arr[i + 1] - arr[i] == 0)
-            continue;
-        else
-            cnt = 1;
-        res = max(res, cnt);
+ // } Driver Code Ends
+class Solution{
+  public:
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+    int findLongestConseqSubseq(int arr[], int N)
+    {
+      //Your code here
+        map<int, bool> mp;
+        
+        for(int i=0;i<N;++i) mp[arr[i]]=true;
+        
+        int curr=0;
+        int ans=0;
+        
+        int prev=mp.begin()->first;
+        
+        for(auto x:mp){
+            if(x.first==prev+1) ++curr;
+            else curr=1;
+            
+            prev=x.first;
+            
+            ans = max(curr, ans);
+        }
+        
+        return ans;
     }
+};
 
-    return res;
-}
-
-inline void solve() {
-    int arr[] = {1, 2, 3, 4, 5, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    cout << findLongestConseqSubseq(arr, n);
-}
+// { Driver Code Starts.
+ 
+// Driver program
+int main()
+{
+ int  t,n,i,a[100001];
+ cin>>t;
+ while(t--)
+ {
+  cin>>n;
+  for(i=0;i<n;i++)
+  cin>>a[i];
+  Solution obj;
+  cout<<obj.findLongestConseqSubseq(a, n)<<endl;
+ }
+      
+    return 0;
+}  // } Driver Code Ends
