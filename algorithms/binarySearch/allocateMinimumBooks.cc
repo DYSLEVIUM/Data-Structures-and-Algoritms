797f7ada3e5c54be1a84d40575496db40c9e7913
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-09-05 20:23:07 +0545

    Create allocateMinimumBooks.cc

---
+++ algorithms/binarySearch/allocateMinimumBooks.cc
@@ -0,0 +1,162 @@
+#define _USE_MATH_DEFINES
+#pragma GCC optimize("Ofast,fast-math,unroll-loops")
+
+#ifdef DYSLEVIUM
+#include "dyslevium.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<ll, ll> pl;
+typedef std::vector<ll> vl;
+typedef std::vector<pl> vpl;
+typedef std::vector<vl> vvl;
+typedef std::map<ll, ll> mll;
+typedef std::priority_queue<ll> pqd;
+typedef std::priority_queue<ll, vl, std::greater<ll>> pqi;
+
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+#define MOD (ll)(1e9 + 7)
+
+std::mt19937_64 RNG(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+//  macro functions
+#define fo(i, n) for (ll i = 0; i < (ll)n; ++i)
+#define Fo(i, k, n) for (ll i = k; k < (ll)n ? i < (ll)n : i > (ll)n; k < (ll)n ? ++i : --i)
+#define all(x) x.begin(), x.end()
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); ++it)
+#define ps(x, y) std::fixed << std::setprecision(y) << x
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define modAdd(a, b) ((((a % MOD) + (b % MOD)) % MOD) + MOD) % MOD
+#define modSub(a, b) ((((a % MOD) - (b % MOD)) % MOD) + MOD) % MOD
+#define modMul(a, b) ((((a % MOD) * (b % MOD)) % MOD) + MOD) % MOD
+
+//  template functions
+template <typename T>
+inline T gcd(const T& a, const T& b) {
+  if (b) return gcd(b, a % b);
+  return a;
+}
+template <typename T>
+inline T binPowIter(T x, T n) {
+  T res = 1;
+  while (n) {
+    if (n & 1) res = modMul(res, x);
+    x = modMul(x, x);
+    n >>= 1;
+  }
+  return res % MOD;
+}
+template <typename T>
+inline T modInverse(const T& a) { return binPowIter(a, MOD - 2); }
+template <typename T>
+inline T modDiv(const T& a, const T& b) { return (modMul(a, modInverse(b)) + MOD) % MOD; }
+
+//  debuging
+#ifdef DYSLEVIUM
+#define deb(x) std::cerr << #x << " = " << x << '\n'
+#else
+#define deb(x)
+#endif
+
+//  initial setup
+inline void setup() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  std::cerr.tie(nullptr);
+
+#ifdef DYSLEVIUM
+  freopen("input.in", "r", stdin);
+  freopen("output.out", "w", stdout);
+  freopen("error.err", "w", stderr);
+#endif
+}
+
+inline void solve();
+
+int main(int argc, char* argv[]) {
+  setup();
+
+  auto startTime = std::chrono::high_resolution_clock::now();
+
+  ll t = 1;
+  // std::cin >> t;
+
+  while (t--) solve();
+
+  auto endTime = std::chrono::high_resolution_clock::now();
+
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+
+#ifdef DYSLEVIUM
+  std::cerr << "\nTime: " << duration.count();
+#endif
+
+  return 0;
+}
+
+using namespace std;
+
+//  Compile and run: g++ -std=c++17 -g -Wshadow -Wall main.cc -D DYSLEVIUM -o a -Ofast -Wno-unused-result && ./a
+
+inline void solve() {
+  //  allocate number of pages so that the maximum total pages is minimum
+  vl arr{10, 20, 30, 40};
+  ll k = 3;
+
+  //  conditions
+  //  1. each student should have at least one book
+  //  2. books can't be shared
+  //  3. assignment of books should be continuous
+
+  if (arr.size() < k) {
+    cout << "NP\n";
+    return;
+  }
+
+  ll sum = accumulate(all(arr), 0);
+  ll maxi = *max_element(all(arr));
+
+  auto isValid = [](const vl& arr, const ll& k, const ll& currMax) {
+    ll cnt = 1;  //  at least one student
+    ll sum = 0;
+
+    fo(i, arr.size()) {
+      if (sum + arr[i] > currMax) {
+        sum = arr[i];
+        ++cnt;
+      } else {
+        sum += arr[i];
+      }
+    }
+
+    return cnt <= k;
+  };
+
+  ll lo = maxi;  //  one book to one student
+  ll hi = sum;   //  all other to other
+
+  ll res = -1;
+  while (lo <= hi) {
+    ll mid = lo + (hi - lo) / 2;
+
+    if (isValid(arr, k, mid)) {
+      res = mid;
+      hi = mid - 1;
+    } else
+      lo = mid + 1;
+  }
+
+  if (res == -1) {
+    cout << "NP\n";
+  } else {
+    cout << res << '\n';
+  }
+}
