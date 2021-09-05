890b794acab4220cd8a96d22afbe7d4fe75ce783
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-09-06 01:00:50 +0545

    Create 0176-nthHighestSalary.sql

---
+++ leetcodeSolutions/0176-nthHighestSalary.sql
@@ -0,0 +1,10 @@
+# there can be no calculation that can happen in arguments supplied to LIMIT
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE M INT;
+SET M=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT IFNULL((SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT M, 1), NULL)
+  );
+END
