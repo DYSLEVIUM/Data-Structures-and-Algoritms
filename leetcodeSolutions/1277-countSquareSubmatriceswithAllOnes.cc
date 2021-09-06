cdb1c10e6e46c71cd227473b908a2bc2e0f23bb8
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-09-06 20:37:23 +0545

    Create 1277-countSquareSubmatriceswithAllOnes.cc

---
+++ leetcodeSolutions/1277-countSquareSubmatriceswithAllOnes.cc
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+
+        int ans = 0;
+
+        for(int i=0;i<matrix.size();++i){
+            for(int j=0;j<matrix[i].size();++j){
+                if(i!=0 && j!=0){
+                    if (matrix[i][j]!=0){
+                        matrix[i][j] = 1+min({matrix[i-1][j-1], matrix[i-1][j], matrix[i][j-1]});
+                    }
+                }
+                
+                ans+=matrix[i][j];
+            }
+        }
+        
+        return ans;
+    }
+};
