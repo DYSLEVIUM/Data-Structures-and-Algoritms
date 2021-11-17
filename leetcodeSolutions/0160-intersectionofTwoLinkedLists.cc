849b8c89cac7757d520e262e87a979b6be308ae8
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-11-17 18:44:03 +0545

    Update 0160-intersectionofTwoLinkedLists.cc

--- leetcodeSolutions/0160-intersectionofTwoLinkedLists.cc
+++ leetcodeSolutions/0160-intersectionofTwoLinkedLists.cc
@@ -9,18 +9,49 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> se;
+//         unordered_set<ListNode*> se;
         
-        while(headA){
-            se.insert(headA);
+//         while(headA){
+//             se.insert(headA);
+//             headA = headA->next;
+//         }
+        
+//         while(headB){
+//             if(se.find(headB)!=se.end()) return headB;
+//             headB = headB->next;
+//         }
+        
+//         return nullptr;
+        
+        auto getLength = [](ListNode* head){
+            int len = 0;
+            while(head->next){
+                head = head->next;
+                ++len;
+            }
+            
+            return len;
+        };
+
+        
+        // making both the lists equidistant from the right end
+        int lenA = getLength(headA), lenB = getLength(headB);
+        
+        while(lenA>lenB){
             headA = headA->next;
+            --lenA;
         }
         
-        while(headB){
-            if(se.find(headB)!=se.end()) return headB;
+        while(lenA<lenB){
+            headB = headB->next;
+            --lenB;
+        }
+        
+        while(headA!=headB){
+            headA = headA->next;
             headB = headB->next;
         }
         
-        return nullptr;
+        return headA;
     }
 };
