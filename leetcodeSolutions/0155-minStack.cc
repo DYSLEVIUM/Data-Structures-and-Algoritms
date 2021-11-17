bcd11ef2d5380d176576a8e40296f1092c9e72ac
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-11-17 18:04:09 +0545

    Create 0155-minStack.cc

---
+++ leetcodeSolutions/0155-minStack.cc
@@ -0,0 +1,32 @@
+class MinStack {
+public:
+    stack<pair<int,int>> st;
+    
+    MinStack() {}
+    
+    void push(int val) {
+        if(this->st.empty()) this->st.push({val, val});
+        else this->st.push({val, min(this->getMin(), val)});
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
