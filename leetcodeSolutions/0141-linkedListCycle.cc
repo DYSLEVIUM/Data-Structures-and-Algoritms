933b97e266ca29f4b82b86248fb63c66fc425fbb
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-11-17 18:22:47 +0545

    Update 0141-linkedListCycle.cc

--- leetcodeSolutions/0141-linkedListCycle.cc
+++ leetcodeSolutions/0141-linkedListCycle.cc
@@ -9,16 +9,31 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_set<ListNode*> se;
-        se.insert(head);
+//         unordered_set<ListNode*> se;
+//         se.insert(head);
         
-        while(head){
-            if(se.find(head->next)!=se.end()) return true;
+//         while(head){
+//             if(se.find(head->next)!=se.end()) return true;
             
-            se.insert(head->next);
-            head = head->next;
+//             se.insert(head->next);
+//             head = head->next;
+//         }
+        
+//         return false;
+        
+        //  0, 1 node, or 2nd node points to nullptr -> cycle is impossible
+        if(head==nullptr || head->next==nullptr ||  head->next->next==nullptr) return false;
+        
+        ListNode* slow = head, *fast = head->next->next;
+        
+        while(slow!=fast){
+            // if we get a nullptr, no cycle can exist
+            if(slow->next == nullptr || fast->next == nullptr || fast->next->next == nullptr) return false;
+            slow = slow->next;
+            fast = fast->next->next;
         }
         
-        return false;
+        //  loop breaks when slow == fast => there is a cycle
+        return true;
     }
 };
