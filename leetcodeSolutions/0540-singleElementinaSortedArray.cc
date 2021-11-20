3fa0fa8bd0b2f105241730178ee5aa51f0283db0
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-11-20 18:17:54 +0545

    Update 0540-singleElementinaSortedArray.cc

--- leetcodeSolutions/0540-singleElementinaSortedArray.cc
+++ leetcodeSolutions/0540-singleElementinaSortedArray.cc
@@ -1,8 +1,6 @@
     int singleNonDuplicate(vector<int>& nums) {
         // int totXor = 0;
-        
         // for(auto num:nums) totXor^=num;
-        
         // return totXor;
     
         int low = 0;
@@ -11,6 +9,7 @@
         while(low<high){
             int mid = low + (high - low)/2;
             
+            //  if mid is even, the next element should be the repeating, else if it is odd, the previous element should be repeating => the single element lies in the other half
             if((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
                 low  = mid+1;
             else
