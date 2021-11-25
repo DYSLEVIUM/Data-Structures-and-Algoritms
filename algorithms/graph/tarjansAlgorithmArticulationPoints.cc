65065a474974a8d7f7fb9f80083b9431488774c5
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-11-25 20:08:56 +0545

    Create tarjansAlgorithmArticulationPoints.cc

---
+++ algorithms/graph/tarjansAlgorithmArticulationPoints.cc
@@ -0,0 +1,162 @@
+#pragma GCC optimize("Ofast,fast-math,unroll-loops")
+#ifdef DYSLEVIUM
+#include "dyslevium.h"
+#else
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<ll, ll> pl;
+typedef std::vector<ll> vl;
+typedef std::vector<pl> vpl;
+typedef std::vector<vl> vvl;
+typedef std::unordered_map<ll, ll> mll;
+typedef std::priority_queue<ll> pqd;
+typedef std::priority_queue<ll, vl, std::greater<ll>> pqi;
+
+// clang-format off
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+#define MOD (ll)(1e9 + 7)
+
+std::mt19937_64 RNG(std::chrono::high_resolution_clock::now().time_since_epoch().count());  // generator for shuffle and other generator which require random numbers
+
+// macro functions
+#define fo(i, n) for (ll i = 0; i < (ll)n; ++i)
+#define Fo(i, k, n) for (ll i = k; k < (ll)n ? i < (ll)n : i > (ll)n; k < (ll)n ? ++i : --i)
+#define all(x) x.begin(), x.end()
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); ++it)
+#define ps(x, y) std::fixed << std::setprecision(y) << x
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define modAdd(a, b) ((((a % MOD) + (b % MOD)) % MOD) + MOD) % MOD
+#define modSub(a, b) ((((a % MOD) - (b % MOD)) % MOD) + MOD) % MOD
+#define modMul(a, b) ((((a % MOD) * (b % MOD)) % MOD) + MOD) % MOD
+
+// template functions
+template <typename T>using ordered_set = __gnu_pbds::tree<T, __gnu_pbds::null_type, std::less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;  // find_by_order, order_of_key
+template <typename T>inline T gcd(const T& a, const T& b) {if (b) return gcd(b, a % b);return a;}
+template <typename T>inline T binPow(T x, T n) {T res = 1;while (n) {if (n & 1) res *= x;x *= x;n >>= 1;}return res;}
+template <typename T>inline T binPowM(T x, T n) {T res = 1;while (n) {if (n & 1) res = modMul(res, x);x = modMul(x, x);n >>= 1;}return res % MOD;}
+template <typename T>inline T modInverse(const T& a) {return binPowIter(a, MOD - 2);}
+template <typename T>inline T modDiv(const T& a, const T& b) {return (modMul(a, modInverse(b)) + MOD) % MOD;}
+
+// debuging
+#ifdef DYSLEVIUM
+#define deb(x) std::cerr << #x << " = " << x << '\n'
+#else
+#define deb(x)
+#endif
+
+// initial setup
+inline void setup() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  #ifdef DYSLEVIUM
+    freopen("input.in", "r", stdin);
+    freopen("output.out", "w", stdout);
+    freopen("error.err", "w", stderr);
+  #endif
+}
+
+inline void solve();
+
+int main(int argc, char* argv[]) {
+  setup();
+
+  auto startTime = std::chrono::high_resolution_clock::now();
+
+  ll t = 1;
+  // std::cin >> t;
+
+  while (t--) solve();
+
+  auto endTime = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+
+  #ifdef DYSLEVIUM
+    std::cerr << "\nTime: " << duration.count();
+  #endif
+
+  return 0;
+}
+
+using namespace std;
+
+// Compile and run: g++ -std=c++17 -g -Wshadow -Wall main.cc -D DYSLEVIUM -o a.exe -Ofast -Wno-unused-result && ./a.exe
+
+// clang-format on
+
+inline void solve() {
+  // a node is a articulation point (cut vertex) if on the removal of the node (and its associated edges), the number of components increases
+
+  //  tarjan's algorithm to find articulation points O(V+E)
+
+  //  case 1: if node U is root of DFS tree and has atleast 2 children (subgraphs)
+
+  //  case 2: if node U is not a root of DFS tree and it has a child V such that no vertex in the subtree rooted with V has a back edge to one of the ancestors of U (excluding U, i.e., parent of V doesn't count as we assume that U is being removed)
+
+  ll const N = 5;
+
+  vvl adj(N);  //  undirected graph
+
+  adj[0].pb(2);
+  adj[2].pb(0);
+  adj[0].pb(3);
+  adj[3].pb(0);
+  adj[1].pb(0);
+  adj[0].pb(1);
+  adj[2].pb(1);
+  // adj[2].pb(4);
+  // adj[4].pb(2);
+  adj[1].pb(2);
+  adj[3].pb(4);
+  adj[4].pb(3);
+
+  vl AP(N, false);  //  initially, none of them are articulation points
+
+  auto find_APs_tarjan = [&AP, &N](const vvl& gr) {
+    vl disc(N, -1);  //  -1 represents not visited
+    vl low_link(N);
+    vl parent(N, -1);  //  -1 represents no parent
+
+    ll time = 0;
+
+    auto dfs = [&](const auto& dfs, const ll& u) -> void {
+      disc[u] = low_link[u] = time++;
+      ll children = 0;
+
+      for (auto v : gr[u]) {
+        if (disc[v] == -1) {
+          ++children;
+          parent[v] = u;
+          dfs(dfs, v);
+          low_link[u] = min(low_link[u], low_link[v]);
+
+          //  case 1: U is root
+          if (parent[u] == -1 && children > 1) AP[u] = true;
+
+          //  case 2: atleast 1 component will get separated
+          if (parent[u] != -1 && low_link[v] >= disc[u]) AP[u] = true;
+        }
+        //  ignoring child to parent edge
+        else if (v != parent[u])  //  this is a back edge, so we are able to reach a previously visited node
+          low_link[u] = min(low_link[u], disc[v]);
+      }
+    };
+
+    fo(i, N) if (disc[i] == -1) dfs(dfs, i);
+  };
+
+  find_APs_tarjan(adj);
+
+  cout << "The Articulation Points are: ";
+  fo(i, N) if (AP[i]) cout << i << ' ';
+}
