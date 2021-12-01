88896d85f95ba7e93e75e78409e47129a1a2a663
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-12-01 19:38:45 +0545

    Create 0011-containerWithMostWater.cc

---
+++ leetcodeSolutions/0011-containerWithMostWater.cc
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i = 0, j=height.size()-1,area = 0;
+        
+        while(i<j){
+            area = max(area, min(height[i],height[j])*(j-i));
+            
+            //  geedily taking the largest height for the container
+            if(height[i]<height[j]) ++i;
+            else --j;
+        }
+        
+        return area;
+    }
+};
