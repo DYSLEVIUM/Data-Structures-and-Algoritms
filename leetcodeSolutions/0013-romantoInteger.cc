204f4fa0cd4ea89ee7953cc5d0c359df75f5ea71
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-12-10 15:05:57 +0545

    Update 0013-romantoInteger.cc

--- leetcodeSolutions/0013-romantoInteger.cc
+++ leetcodeSolutions/0013-romantoInteger.cc
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int romanToInt(string s) {
-      unordered_map<char,int> mp;
+        unordered_map<char,int> mp;
       
-      mp['I']=1;
-      mp['V']=5;
-      mp['X']=10;
-      mp['L']=50;
-      mp['C']=100;
-      mp['D']=500;
-      mp['M']=1000;
+        mp['I']=1;
+        mp['V']=5;
+        mp['X']=10;
+        mp['L']=50;
+        mp['C']=100;
+        mp['D']=500;
+        mp['M']=1000;
+
+        int n = s.length();
+        int ans = mp[s.back()];
+        
+        for(int i=0;i<n-1;++i){
+            if(mp[s[i]]<mp[s[i+1]]) ans -= mp[s[i]];
+            else ans += mp[s[i]];
+        }
       
-      int ans = mp[s[0]];
-      
-      for(int i=1;i<s.length();++i){
-        if(mp[s[i]]<=mp[s[i-1]]) ans+=mp[s[i]];
-        else ans+=(mp[s[i]]-2*mp[s[i-1]]);
-      }
-      
-      return ans;
+        return ans;
     }
 };
