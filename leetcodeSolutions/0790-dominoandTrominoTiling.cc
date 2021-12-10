424687ef7e56a3212445133e7e7ddd3d22150623
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-12-10 14:57:45 +0545

    Update 0790-dominoandTrominoTiling.cc

--- leetcodeSolutions/0790-dominoandTrominoTiling.cc
+++ leetcodeSolutions/0790-dominoandTrominoTiling.cc
@@ -12,15 +12,15 @@ public:
         //  f signifies fully filled
         //  p signifies partially filled
         
-        int f_prev = 1;
-        int f_curr = 2;
-        int p_curr = 1;
+        int f_prev = 1; //  at 1
+        int f_curr = 2; //  at 2
+        int p_curr = 1; //  at 2
         
         for(int i = 3; i < n + 1; ++i){
             int temp = f_curr;
             
-            f_curr = mod_add(mod_add(mod_add(f_curr, f_prev), 2 * p_curr) , 0);
-            p_curr = mod_add(mod_add(p_curr, f_prev), 0);
+            f_curr = mod_add(mod_add(mod_add(f_curr, f_prev), 2 * p_curr) , 0); //  make full board by adding a vertical and adding 2 horizontal to i-1 and a L piece (multiply by 2 due to horizontal symmetry) to partial board
+            p_curr = mod_add(mod_add(p_curr, f_prev), 0);   //  make another partial board by adding a horizontal piece
             
             f_prev = temp;
         }
