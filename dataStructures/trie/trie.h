4746f912770abddef92382d8234fb8d537b1e563
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-12-17 17:48:37 +0545

    Update trie.h

--- dataStructures/trie/trie.h
+++ dataStructures/trie/trie.h
@@ -1,100 +1,58 @@
 class Trie {
   class Node {
-    bool isLeaf;
-
+    bool is_leaf;
     unordered_map<char, Node*> mp;
 
-    Node() : isLeaf(false) {}
+    Node() : is_leaf(false) {}
 
     virtual ~Node() {
-      for (auto it : mp) {
-        delete it.S;
-        it.S = nullptr;
+      for (auto& it : mp) {
+        delete it.second;
+        it.second = nullptr;
       }
     }
 
     friend class Trie;
   };
 
-  bool haveChildren() {
-    for (auto it : this->root->mp) {
-      if (it.S != nullptr) return true;
+  bool have_children() {
+    for (auto& it : this->root->mp) {
+      if (it.second != nullptr) return true;
     }
-
     return false;
   }
 
- public:
   Node* root;
 
+ public:
   Trie() : root(nullptr) {}
 
   void insert(const string& data) {
-    //  if the trie is intially empty
     if (this->root == nullptr) this->root = new Node();
 
-    Node* currNode = this->root;
+    Node* curr_node = this->root;
 
-    fo(i, data.size()) {
-      //  create a node if the path doesn't exist
-      if (currNode->mp.find(data[i]) == currNode->mp.end()) {
-        currNode->mp[data[i]] = new Node();
-      }
+    for (auto& ch : data) {
+      if (curr_node->mp.find(ch) == curr_node->mp.end()) curr_node->mp[ch] = new Node();
 
-      //  go to next node
-      currNode = currNode->mp[data[i]];
+      curr_node = curr_node->mp[ch];
     }
 
-    //  after inserting the word, it is valid
-    currNode->isLeaf = true;
+    curr_node->is_leaf = true;
   }
 
   bool find(const string& data) {
     if (this->root == nullptr) return false;
 
-    Node* currNode = this->root;
+    Node* curr_node = this->root;
 
-    fo(i, data.size()) {
-      //  if node doesn't exist, data is not present
-      if (currNode->mp.find(data[i]) == currNode->mp.end()) return false;
+    for (auto& ch : data) {
+      if (curr_node->mp.find(ch) == curr_node->mp.end()) return false;
 
-      currNode = currNode->mp[data[i]];
+      curr_node = curr_node->mp[ch];
     }
 
-    return currNode->isLeaf;
-  }
-
-  void remove(const string& data) {
-    if (this->root == nullptr) return;
-
-    auto removeHelper = [&](const auto& removeHelper, Node* currNode, string data, ll idx = 0) -> bool {
-      //  end of the string
-      if ((ll)data.size() == idx && currNode->isLeaf) {
-        if (!this->haveChildren()) {
-          delete currNode;  //  delete the current node
-          currNode = nullptr;
-          return true;  //  delete the non-leaf parent nodes
-        } else {
-          //  mark the current node as a non-leaf node as this is not the end point any more
-          currNode->isLeaf = false;
-          return false;  //  don't delete its parent nodes
-        }
-      } else {
-        // recur for the node corresponding to the next character in the string and if it returns true, delete the current node if it is non-leaf
-        if (currNode->mp.find(data[idx]) != currNode->mp.end() && removeHelper(removeHelper, currNode->mp[data[idx]], data, idx + 1) && currNode->isLeaf == false) {
-          if (!this->haveChildren()) {
-            delete currNode;
-            currNode = nullptr;
-            return true;  //  delete the non-leaf parent nodes
-          } else
-            return false;  //  don't delete its parent nodes
-        }
-      }
-
-      return false;
-    };
-
-    removeHelper(removeHelper, this->root, data, 0);
+    return curr_node->is_leaf;
   }
 
   ~Trie() {
