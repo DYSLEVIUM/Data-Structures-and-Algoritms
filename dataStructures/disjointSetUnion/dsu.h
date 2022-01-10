9be7bb8364d195793bbb486187a297afc76040da
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-01-10 15:39:09 +0545

    Update dsu.h

--- dataStructures/disjointSetUnion/dsu.h
+++ dataStructures/disjointSetUnion/dsu.h
@@ -8,10 +8,10 @@ class DSU {
   // store other info as required
   DSU(const size_t& n) {
     this->rank.resize(n);
-    fill(all(this->rank), 0);
+    fill(this->rank.begin(), this->rank.end(), 0);
 
     this->parent.resize(n);
-    iota(all(this->parent), 0);
+    iota(this->parent.begin(), this->parent.end(), 0);
   }
 
   T find(const T& node) {
@@ -19,17 +19,17 @@ class DSU {
     return this->parent[node] = (this->parent[node] == node ? node : this->find(this->parent[node]));
   }
 
-  void merge(T node1, T node2) {
-    node1 = this->find(node1);
-    node2 = this->find(node2);
+  void merge(T node_1, T node_2) {
+    node_1 = this->find(node_1);
+    node_2 = this->find(node_2);
 
     //  both the nodes are present in the same set
-    if (node1 == node2) return;
+    if (node_1 == node_2) return;
 
     //  either of the node can be the parent, so rank will increase
-    if (this->rank[node1] == this->rank[node2]) ++(this->rank[node1]);
+    if (this->rank[node_1] == this->rank[node_2]) ++this->rank[node_1];
 
     //  higher rank will be the parent (union by rank to reduce complexity)
-    this->rank[node1] < this->rank[node2] ? this->parent[node1] = node2 : this->parent[node2] = node1;
+    this->rank[node_1] < this->rank[node_2] ? this->parent[node_1] = node_2 : this->parent[node_2] = node_1;
   }
 };
