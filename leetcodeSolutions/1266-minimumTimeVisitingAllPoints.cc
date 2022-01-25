f4819a36bc2f9e11ddb0cbeb16ced82c2e6211b7
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-01-25 20:25:10 +0545

    Create 1266-minimumTimeVisitingAllPoints.cc

---
+++ leetcodeSolutions/1266-minimumTimeVisitingAllPoints.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+      int ans = 0;
+      int n = points.size();
+      
+      for(int i=1;i<n;++i){
+        int optimal = min(abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1]));
+        ans+=abs(points[i][0]-points[i-1][0]) + abs(points[i][1]-points[i-1][1]) - optimal;
+      }
+      
+      return ans;
+    }
+};
