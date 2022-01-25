79d932e9ecb599b6cb1b1dd82ddb5acf8f9c2abe
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-01-25 20:27:07 +0545

    Update 1266-minimumTimeVisitingAllPoints.cc

--- leetcodeSolutions/1266-minimumTimeVisitingAllPoints.cc
+++ leetcodeSolutions/1266-minimumTimeVisitingAllPoints.cc
@@ -4,10 +4,12 @@ public:
       int ans = 0;
       int n = points.size();
       
-      for(int i=1;i<n;++i){
-        int optimal = min(abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1]));
-        ans+=abs(points[i][0]-points[i-1][0]) + abs(points[i][1]-points[i-1][1]) - optimal;
-      }
+//       for(int i=1;i<n;++i){
+//         int optimal = min(abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1]));
+//         ans+=abs(points[i][0]-points[i-1][0]) + abs(points[i][1]-points[i-1][1]) - optimal;
+//       }
+        
+        for(int i=1;i<n;++i)ans+=max(abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1]));
       
       return ans;
     }
