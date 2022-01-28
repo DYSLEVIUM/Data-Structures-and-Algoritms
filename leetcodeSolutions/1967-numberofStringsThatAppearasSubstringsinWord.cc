6412e1f55ff1c08cfc741fc9b92b8d119bc5ba22
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-01-29 00:25:58 +0545

    Create 1967-numberofStringsThatAppearasSubstringsinWord.cc

---
+++ leetcodeSolutions/1967-numberofStringsThatAppearasSubstringsinWord.cc
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+      int ans = 0;
+      for(auto& pattern: patterns) ans += word.find(pattern) != string::npos;
+      return ans;
+    }
+};
