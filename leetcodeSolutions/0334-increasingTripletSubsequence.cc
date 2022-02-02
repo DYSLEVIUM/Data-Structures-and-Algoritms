131e257897b78ca03b6e4dac70a9363fd29779ae
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-02-02 13:01:12 +0545

    Update 0334-increasingTripletSubsequence.cc

--- leetcodeSolutions/0334-increasingTripletSubsequence.cc
+++ leetcodeSolutions/0334-increasingTripletSubsequence.cc
@@ -3,6 +3,7 @@ public:
     bool increasingTriplet(vector<int>& nums) {
       int maxx = INT_MAX, minn = INT_MAX;
       
+      //  we will find the minimum number and the just greater number, if it is not from the max or min, that means it is the middle element of those two elements
       for(auto& num:nums){
         if(num<=minn) minn = num;
         else if(num<=maxx) maxx = num;
