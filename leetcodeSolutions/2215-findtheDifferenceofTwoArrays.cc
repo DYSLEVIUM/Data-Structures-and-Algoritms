c3ef99c92b52e7589c96e782529496ba9cff8d34
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-03-27 13:34:30 +0530

    Create 2215-findtheDifferenceofTwoArrays.cc

---
+++ leetcodeSolutions/2215-findtheDifferenceofTwoArrays.cc
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+      set<int> te;
+      
+      for(auto& num: nums2) te.insert(num);
+
+      vector<int> ans1;
+      for(auto& num: nums1) {
+        if(te.find(num) == te.end()) {
+          te.insert(num);
+          ans1.push_back(num);
+        }
+      }
+      
+      te.clear();
+    
+      for(auto& num: nums1) te.insert(num);
+
+      vector<int> ans2;
+      for(auto& num: nums2) {
+        if(te.find(num) == te.end()) {
+          te.insert(num);
+          ans2.push_back(num);
+        }
+      }
+      
+      return {ans1, ans2};
+    }
+};
