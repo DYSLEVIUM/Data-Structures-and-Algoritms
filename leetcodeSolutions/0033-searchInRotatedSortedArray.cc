a0b246d73fd8b7982e63360e65f616c75c9d2275
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-04-10 15:31:43 +0530

    Update 0033-searchInRotatedSortedArray.cc

--- leetcodeSolutions/0033-searchInRotatedSortedArray.cc
+++ leetcodeSolutions/0033-searchInRotatedSortedArray.cc
@@ -1,26 +1,28 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int lo = 0, hi = nums.size()-1;
+      int n = nums.size();
+      
+      int lo = 0, hi = n - 1;
+      while(lo <= hi) {
+        int mid = lo + ((hi - lo) >> 1);
         
-        while(lo <= hi){
-            int mid = lo + (hi - lo) / 2;
-            
-            if(nums[mid] == target){
-                return mid;
-            }else if(nums[mid] < nums[hi]){ //  if the right half is sorted, we can say if the target is present in that range or not
-                if(target > nums[mid] && target <= nums[hi]) 
-                    lo = mid + 1;
-                else
-                    hi = mid - 1;
-            }else{  //  nums[lo]>nums[mid]
-                if (target >= nums[lo] && target < nums[mid])
-                    hi = mid - 1;
-                else
-                    lo = mid + 1; 
-            }
+        if(nums[mid] == target) return mid;
+        
+        //  is the left half sorted?
+        if(nums[lo] <= nums[mid]) {
+          //  does the target lie in the first half?
+          if(target >= nums[lo] && target < nums[mid]) hi = mid - 1;
+          else lo = mid + 1;
+        } 
+        //  if the left half is not sorted, the right half is definately sorted, otherwise there is a contradition of it not being a rotated sorted array
+        else{
+          //  does the target lie in the second half?
+          if(target > nums[mid] && target <= nums[hi]) lo = mid + 1;
+          else hi = mid - 1;
         }
-        
-        return -1;
+      }
+      
+      return -1;
     }
 };
