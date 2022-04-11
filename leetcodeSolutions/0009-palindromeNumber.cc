1b75aa7d5c379a4f33297a0b5199196a605f95ab
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-04-11 14:50:49 +0530

    Create 0009-palindromeNumber.cc

---
+++ leetcodeSolutions/0009-palindromeNumber.cc
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+      string s = to_string(x);
+      for(int i = 0; i < s.length() / 2; ++i) {
+        if(s[i] != s[s.length() - i - 1]) return false;
+      }
+      return true;
+    }
+};
