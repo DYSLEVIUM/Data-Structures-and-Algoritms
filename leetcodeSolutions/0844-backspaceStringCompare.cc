47cfaadaad3d7b6583b2d8ed0264a564200769be
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-05-01 23:09:25 +0530

    Update 0844-backspaceStringCompare.cc

--- leetcodeSolutions/0844-backspaceStringCompare.cc
+++ leetcodeSolutions/0844-backspaceStringCompare.cc
@@ -1,19 +1,37 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        auto get_new_str = [](const string &str){
-            string new_str = "";
-            for(auto &ch: str) {
-                if(ch == '#') {
-                    if(new_str.length()) new_str.pop_back();
-                    continue;
+        int ptr_s = s.length() - 1, ptr_t = t.length() - 1;
+        int skip_s = 0, skip_t = 0;
+        
+        while(ptr_s >= 0 || ptr_t >= 0) {
+            while(ptr_s >= 0) {
+                if(s[ptr_s] == '#') {
+                    ++skip_s, --ptr_s;
+                } else if(skip_s > 0) {
+                    --skip_s, --ptr_s;
+                } else {
+                    break;
                 }
-                new_str += ch;
             }
             
-            return new_str;
-        };
-
-        return get_new_str(s) == get_new_str(t);
+            while(ptr_t >= 0) {
+                if(t[ptr_t] == '#') {
+                    ++skip_t, --ptr_t;
+                } else if(skip_t > 0) {
+                    --skip_t, --ptr_t;
+                } else {
+                    break;
+                }
+            }
+            
+            if(ptr_s >= 0 && ptr_t >= 0 && s[ptr_s] != t[ptr_t]) return false;
+            
+            if((ptr_s >= 0) != (ptr_t >= 0)) return false;
+            
+            --ptr_s, --ptr_t;
+        }
+        
+        return true;
     }
 };
