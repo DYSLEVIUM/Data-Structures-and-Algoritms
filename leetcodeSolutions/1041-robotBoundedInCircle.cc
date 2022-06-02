4dceb5dff3594d503d1637043f5adf1c9372793b
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-06-02 18:10:09 +0530

    Create 1041-robotBoundedInCircle.cc

---
+++ leetcodeSolutions/1041-robotBoundedInCircle.cc
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        pair<int, int> pos = {0, 0};    //  position vector
+        pair<int, int> dir = {0, 1};    //  direction unit vector
+        
+        for(char &instruction: instructions) {
+            if(instruction == 'G') {
+                pos.first += dir.first;
+                pos.second += dir.second;
+            } 
+            //  we will rotate the plane w.r.t the robot
+            else if(instruction == 'L') {
+                //  when we rotate the plane left, the +x becomes, +y, and, +y becomes -x
+                swap(dir.first, dir.second);
+                dir.first *= -1;
+            } else {
+                //  when we rotate the plane right, the +x becomes, -y, and, +y becomes +x
+                swap(dir.first, dir.second);
+                dir.second *= -1;
+            }
+        }
+        
+        //  if position has not been changed or the direction(independent of position) changed, we have a cycle
+        return (pos == make_pair(0, 0) || dir != make_pair(0, 1));
+    }
+};
