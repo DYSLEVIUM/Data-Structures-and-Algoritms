e17b9b633e6f3aee845261c1472843cbc58477ee
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-06-03 16:48:23 +0530

    Update 2284-senderWithLargestWordCount.cc

--- leetcodeSolutions/2284-senderWithLargestWordCount.cc
+++ leetcodeSolutions/2284-senderWithLargestWordCount.cc
@@ -3,63 +3,20 @@ public:
     string largestWordCount(vector<string>& messages, vector<string>& senders) {
         int n = messages.size();
         
-        auto strcmp = [](const string &a, const string &b){
-            int a_cnt = 0, b_cnt = 0;
-            
-            if(a == b) return 0;
-            
-            while(a_cnt < a.length() && b_cnt < b.length()) {
-                if(a[a_cnt] < b[b_cnt]) return -1;
-                else if(a[a_cnt] > b[b_cnt]) return 1;
-                ++a_cnt, ++b_cnt;
-            }
-            
-            if(a_cnt < b_cnt) return -1;
-            
-            return 1;
-        };
+        unordered_map<string, int> cnt;
         
-        auto get_count = [](string s){
-            while(s.back() == ' ') {
-                s.pop_back();
-            }
-            
-            reverse(s.begin(), s.end());
-            while(s.back() == ' ') {
-                s.pop_back();
-            }
-
-            reverse(s.begin(), s.end());
-
-            int cnt = 0;
-            for(int i = 0; i < s.length(); ++i) {
-                cnt += s[i] == ' ';
-            }
-            
-            return cnt + 1;
-        };
-        
-        unordered_map<string, int> mp;
+        string res;
+        int mx_cnt = 0;
         for(int i = 0; i < n; ++i) {
-            mp[senders[i]] += get_count(messages[i]);
-        }
-        
-        string ans;
-        int curr_cnt = INT_MIN;
-        
-        for(auto &[sender, cnt]: mp) {
-            if(curr_cnt <= cnt) {
-                if(curr_cnt == cnt) {
-                    if(strcmp(sender, ans) >= 0) {
-                        ans = sender;
-                    }   
-                } else {
-                    ans = sender;
-                    curr_cnt = cnt;    
-                }
+            int words = count(messages[i].begin(), messages[i].end(), ' ') + 1;
+            int total = cnt[senders[i]] += words;
+            
+            if(total > mx_cnt || (total == mx_cnt && senders[i] > res)) {
+                mx_cnt = total;
+                res = senders[i];
             }
         }
         
-        return ans;
+        return res;
     }
 };
