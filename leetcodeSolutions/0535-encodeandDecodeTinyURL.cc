c088256e94f750878737776dc1a7df67468225df
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-06-11 15:47:50 +0530

    Update 0535-encodeandDecodeTinyURL.cc

--- leetcodeSolutions/0535-encodeandDecodeTinyURL.cc
+++ leetcodeSolutions/0535-encodeandDecodeTinyURL.cc
@@ -1,15 +1,26 @@
 class Solution {
 public:
-
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        return longUrl;
+        srand(time(NULL));
+        
+        string shortUrl = "";
+        for(int i = 0; i < m_short_len; ++i) {
+            shortUrl += m_char_set[rand() % m_char_set.length()];
+        }
+        
+        m_hashmap[shortUrl] = longUrl;
+        return shortUrl;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        return m_hashmap[shortUrl];
     }
+private:
+    const string m_char_set = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    const int m_short_len = 6;
+    unordered_map<string, string> m_hashmap;
 };
 
 // Your Solution object will be instantiated and called as such:
