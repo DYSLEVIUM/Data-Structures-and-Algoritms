fe94685f1d4f25d8c5f202140c0471cb07418982
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-06-21 17:56:05 +0545

    Create 0647-palindromicSubstrings.cc

---
+++ leetcodeSolutions/0647-palindromicSubstrings.cc
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+      int n = s.length();
+      int cnt = 0;
+      
+      auto expand = [&](int le, int ri){
+        while(le >= 0 && ri < n && s[le] == s[ri]) {
+          --le, ++ri, ++cnt;
+        }
+        ++le, --ri;
+      };
+      
+      for(int i = 0; i < n; ++i) {
+        //  for odd length
+        expand(i, i);
+        //  for even length
+        expand(i, i + 1);
+      }
+      
+      return cnt;
+    }
+};
