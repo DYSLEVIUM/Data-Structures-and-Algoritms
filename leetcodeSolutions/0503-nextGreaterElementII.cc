f98c7de70681a91d4d127572f082bb3cabf0ddb0
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-07-02 19:21:14 +0530

    Create 0503-nextGreaterElementII.cc

---
+++ leetcodeSolutions/0503-nextGreaterElementII.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+      int n = nums.size();
+      
+      stack<int> st;
+      vector<int> ans(n);
+      for(int i = 2 * n - 1; i >= 0; --i) {
+        while(!st.empty() && nums[i % n] >= st.top()) {
+          st.pop();
+        }
+        
+        ans[i % n] = st.empty() ? -1 : st.top();
+        
+        st.push(nums[i % n]);
+      }
+      
+      return ans;
+    }
+};
