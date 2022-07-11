f5279efc8f11544cff2ec15185208dd536b90b80
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-07-11 17:50:24 +0530

    Create 1944-numberofVisiblePeopleinaQueue.cc

---
+++ leetcodeSolutions/1944-numberofVisiblePeopleinaQueue.cc
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> canSeePersonsCount(vector<int>& heights) {
+      int n = heights.size();
+      
+      vector<int> ans(n);
+      
+      // monotonic queue
+//       deque<int> max_queue;
+//       for(int i = n - 1; i >= 0; --i) {
+//         while(!max_queue.empty() && heights[max_queue.back()] <= heights[i]) {
+//           max_queue.pop_back();
+//           ++ans[i];
+//         }
+//         //  the just next person's height doesn't matter
+//         ans[i] += !max_queue.empty();
+        
+//         max_queue.push_back(i);
+//       }
+      
+      //  how many next greater elements are there
+      
+      //  monotonic stack (v1)
+//       stack<int> st;
+//       for(int i = 0; i < n; ++i) {
+//         while(!st.empty() && heights[st.top()] <= heights[i]) {
+//           ++ans[st.top()];
+//           st.pop();
+//         }
+        
+//         //  the just next person's height doesn't matter
+//         if(!st.empty()) {
+//           ++ans[st.top()];
+//         }
+        
+//         st.push(i);
+//       }
+      
+      //  monotonic stack (v2)
+      stack<int> st;
+      for(int i = n - 1; i >= 0; --i) {
+        while(!st.empty() && heights[st.top()] <= heights[i]) {
+          ++ans[i];
+          st.pop();
+        }
+        
+        //  the just next person's height doesn't matter
+        ans[i] += !st.empty();
+        
+        st.push(i);
+      }
+      
+      return ans;
+    }
+};
