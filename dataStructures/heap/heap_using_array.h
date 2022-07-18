849057641f7b1f8c9e8f8304b160ccf291ac1c57
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-07-18 20:32:31 +0530

    Update heap_using_array.h

--- dataStructures/heap/heap_using_array.h
+++ dataStructures/heap/heap_using_array.h
@@ -1,64 +1,68 @@
-//  it must be a complete binary tree, i.e., left child should be filled before the right child
-template <typename T, typename __comp = std::less<T>>
-class Heap {
- private:
-  uint64_t _size;
-  vector<T> _data;
+/*  it must be a complete binary tree, i.e., left child should be filled before
+ * the right child */
+template <typename T, typename __comp = std::less<T>> class Heap {
+public:
+  Heap() : m_size(0){};
+
+  bool empty() const { return m_size == 0; };
+  int size() const { return m_size; };
+  T top() const { return m_data[0]; };
+
+  void push(const T &curr_data) {
+    //  making space to insert
+    if (m_size + 1 >= m_data.size())
+      m_data.push_back(-1);
+
+    m_data[m_size++] = curr_data;
+
+    heapify(0);
+  };
+
+  void pop() {
+    if (!m_size) {
+      return;
+    }
+    //  putting the last element at top
+    std::swap(m_data[0], m_data[--m_size]);
+
+    m_data.pop_back(); //  deleting the node
+
+    heapify(0);
+  };
+
+private:
+  int m_size;
+  std::vector<T> m_data;
   __comp cmp = __comp{};
 
-  uint64_t parent(const uint64_t& i) const { return i >> 1; };
-  uint64_t left_child(const uint64_t& i) const { return (i << 1) + 1; };
-  uint64_t right_child(const uint64_t& i) const { return (i << 1) + 2; };
-  bool node_exists(const uint64_t& i) const { return i < this->_size; };
+  int parent(const int &i) const { return i >> 1; };
+  int left_child(const int &i) const { return (i << 1) + 1; };
+  int right_child(const int &i) const { return (i << 1) + 2; };
+  bool node_exists(const int &i) const { return i < m_size; };
 
-  //  we always heapify the subtrees, this is different from the create_heap where on insertion, we used to heap up (for n nodes, we have to bubble up with height of log n, so time complexity is O(n log n)) and on deletion we used to heap down
-  void heapify(const uint16_t& idx) {  //  heap down implementation
-    if (idx >= this->_size) return;
+  //  we always heapify the subtrees, this is different from the create_heap
+  //  where on insertion, we used to heap up (for n nodes, we have to bubble up
+  //  with height of log n, so time complexity is O(n log n)) and on deletion we
+  //  used to heap down
+  void heapify(const uint16_t &idx) { //  heap down implementation
+    if (idx >= m_size)
+      return;
 
-    uint64_t left_idx = left_child(idx);
-    uint64_t right_idx = right_child(idx);
+    int left_idx = left_child(idx);
+    int right_idx = right_child(idx);
 
     //  first on the leaf nodes
     heapify(left_idx);
     heapify(right_idx);
 
-    if (node_exists(left_idx) && cmp(this->_data[idx], this->_data[left_idx])) {
-      swap(this->_data[left_idx], this->_data[idx]);
+    if (node_exists(left_idx) && cmp(m_data[idx], m_data[left_idx])) {
+      std::swap(m_data[left_idx], m_data[idx]);
       heapify(left_idx);
     }
 
-    if (node_exists(right_idx) && cmp(this->_data[idx], this->_data[right_idx])) {
-      swap(this->_data[right_idx], this->_data[idx]);
+    if (node_exists(right_idx) && cmp(m_data[idx], m_data[right_idx])) {
+      std::swap(m_data[right_idx], m_data[idx]);
       heapify(right_idx);
     }
-  };
-
- public:
-  Heap() : _size(0){};
-
-  bool is_empty() const { return this->_size == 0; };
-
-  uint64_t size() const { return this->_size; };
-
-  T top() const { return this->_data[0]; };
-
-  void push(const T& da) {
-    //  making space to insert
-    if (this->_size + 1 >= this->_data.size()) this->_data.push_back(-1);
-
-    this->_data[this->_size++] = da;
-
-    this->heapify(0);
-  };
-
-  void pop() {
-    if (!this->_size) return;
-
-    //  putting the last element at top
-    swap(this->_data[0], this->_data[--this->_size]);
-
-    this->_data.pop_back();  //  deleting the node
-
-    this->heapify(0);
   };
 };
