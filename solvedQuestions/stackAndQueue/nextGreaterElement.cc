ce5496f2f65086adea87fe742a6c4b731b8465c7
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-07-29 22:09:23 +0530

    Create nextGreaterElement.cc

--- solvedQuestions/stackAndQueue/nextGreaterElement.cc
+++ solvedQuestions/stackAndQueue/nextGreaterElement.cc
@@ -1,83 +1,54 @@
-#pragma GCC optimize("Ofast,fast-math")
-
-#include <bits/stdc++.h>
-
-#define deb(x) cout << '\n' \
-                    << #x << " = " << x << '\n'
-
-inline void setup() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
-
-#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
-    freopen("input.txt", "r", stdin);
-#else
-#ifndef ONLINE_JUDGE  // runs automatically for supported online judges
-    freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-#endif
-#endif
-}
-
-inline void solve();
-
-int main(int argc, char* argv[]) {
-    setup();
-
-    long long t = 1;
-    // std::cin >> t;
-
-    while (t--)
-        solve();
-
-    return 0;
-}
-
-//Compile: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result
-//Build: g++ -g -Wshadow -Wall main.cc -o a.exe -D_GLIBCXX_DEBUG
-//Compile and run: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result && a.exe
-
+//{ Driver Code Starts
+#include<bits/stdc++.h>
 using namespace std;
 
-//  https://practice.geeksforgeeks.org/problems/next-larger-element-1587115620/1#
 
-vector<long long> nextLargerElement(vector<long long> arr, int n) {
-    vector<long long> res(n);
-    stack<long long> s;
-
-    for (long long i = n - 1; i >= 0; --i) {
-        if (s.empty()) {
-            res[i] = -1;
-            s.push(arr[i]);
-        } else if (arr[i] < s.top()) {
-            res[i] = s.top();
-            s.push(arr[i]);
-        } else if (arr[i] > s.top()) {
-            while (!s.empty() && arr[i] > s.top()) {
-                s.pop();
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        stack<int> st;
+        vector<long long> ans(n);
+        for(int i = n - 1; i >= 0; --i) {
+            while(!st.empty() && arr[st.top()] < arr[i]) {
+                st.pop();
             }
-
-            if (s.empty()) {
-                res[i] = -1;
-                s.push(arr[i]);
-            } else {
-                res[i] = s.top();
-                s.push(arr[i]);
+            
+            long long val = -1;
+            if(!st.empty()) {
+                val = arr[st.top()];
             }
+            ans[i] = val;
+            
+            st.push(i);
         }
+        
+        return ans;
     }
+};
 
-    return res;
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        int n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution obj;
+        vector <long long> res = obj.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
+    }
+	return 0;
 }
-
-inline void solve() {
-    int n;
-    cin >> n;
-    vector<long long> arr(n);
-    for (int i = 0; i < n; ++i) cin >> arr[i];
-
-    vector<long long> res = nextLargerElement(arr, n);
-    for (long long i : res) cout << i << ' ';
-    cout << '\n';
-}
+// } Driver Code Ends
