4de1b8b6bda4d0fb2fb2391498e4144dffdfe9a1
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-07-30 15:04:21 +0530

    Create printAnagramsTogether.cc

---
+++ solvedQuestions/string/printAnagramsTogether.cc
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        unordered_map<string, vector<string>> mp;
+        
+        for(auto &s : string_list) {
+            string te = s;
+            sort(te.begin(), te.end());
+            
+            mp[te].push_back(s);
+        }
+        
+        vector<vector<string>> ans;
+        for(auto &it : mp) {
+            ans.push_back(it.second);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
