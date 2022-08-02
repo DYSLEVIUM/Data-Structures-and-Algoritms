d19c6fb052ac05d56f304228931987918b88ae7a
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-08-02 17:14:41 +0530

    Create 0319-bulbSwitcher.cc

---
+++ leetcodeSolutions/0319-bulbSwitcher.cc
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        // bulbs will be on, if d divides i an odd number of times
+        // divisors come in pairs, so we will always have an even number of divisors except when x is perfect square, so we need to count the number of perfect squares from 1 to n which is sqrt(n)
+        return sqrt(n);
+    }
+};
