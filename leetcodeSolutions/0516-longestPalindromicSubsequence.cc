7cda335706da1a3c07ae9a361d26542bb71a6de7
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-08-02 11:31:14 +0530

    Update 0516-longestPalindromicSubsequence.cc

--- leetcodeSolutions/0516-longestPalindromicSubsequence.cc
+++ leetcodeSolutions/0516-longestPalindromicSubsequence.cc
@@ -1,26 +1,58 @@
+// using lcs method
+// class Solution {
+// public:
+//     int longestPalindromeSubseq(string s) {
+//       string t = s;
+//       reverse(t.begin(), t.end());
+      
+//       auto lcs = [](const string& x, const string& y){
+//         int n1 = x.length(), n2 = y.length();
+//         vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1));
+        
+//         for(int i = 1; i < n1 + 1; ++i) {
+//           for(int j = 1; j < n2 + 1; ++j) {
+//             if(x[i - 1] == y[j - 1]) {
+//               dp[i][j] = 1 + dp[i - 1][j - 1];
+//             } else {
+//               dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//             }
+//           }
+//         }
+      
+//         return dp[n1][n2];
+//       };
+      
+//       return lcs(s, t);
+//     }
+// };
+
+// using gap method
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
-      string t = s;
-      reverse(t.begin(), t.end());
-      
-      auto lcs = [](const string& x, const string& y){
-        int n1 = x.length(), n2 = y.length();
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1));
+        int n = s.length();
         
-        for(int i = 1; i < n1 + 1; ++i) {
-          for(int j = 1; j < n2 + 1; ++j) {
-            if(x[i - 1] == y[j - 1]) {
-              dp[i][j] = 1 + dp[i - 1][j - 1];
-            } else {
-              dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        vector<vector<int>> dp(n, vector<int>(n));
+        for(int gap = 0; gap < n; ++gap) {
+            for(int i = 0, j = gap; j < n; ++i, ++j) {
+                if(!gap) {
+                    // initialization
+                    dp[i][j] = 1;
+                    continue;
+                }
+                // this if is not needed as we only need dp value at i >= j, which is covered in the next if condition, i.e., if j > i, we get 2 + 0, which is what we wanted
+                // if(gap == 2) {
+                //     dp[i][j] = s[i] == s[j] ? 2 : 1;
+                //     continue;
+                // }
+                if(s[i] == s[j]) {
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+                }
             }
-          }
         }
-      
-        return dp[n1][n2];
-      };
-      
-      return lcs(s, t);
+        
+        return dp[0][n - 1];
     }
 };
