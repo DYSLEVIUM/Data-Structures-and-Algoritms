69b9458109286fe9ea229219209eedb094b554b2
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-08-10 15:56:45 +0530

    Create maximumPathSumbetween2LeafNodes.cc

---
+++ solvedQuestions/binaryTrees/maximumPathSumbetween2LeafNodes.cc
@@ -0,0 +1,146 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* temp;
+    int ans = INT_MIN;
+    
+    int rec(Node * node){
+        if(!node) {
+            return 0;
+        }
+        
+        int le = rec(node->left);
+        int ri = rec(node->right);
+        
+        // if the main root doesn't have a right or left, then we consider the root as a leaf
+        if(node == temp && (!node->left || !node->right)) {
+            ans = max(ans, node->data + le + ri);
+        }
+        
+        if(node->left && node->right) {
+            // the path
+            ans = max(ans, node->data + le + ri);
+            
+            // part of the path
+            return node->data + max(le, ri);
+        }
+        
+        // part of the path as only either the left or the right is null
+        return node->data + le + ri;
+    }
+    
+    int maxPathSum(Node* root) {
+        temp = root;
+        rec(root);
+        return ans;        
+    }
+    
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        Solution ob;
+        cout << ob.maxPathSum(root) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
