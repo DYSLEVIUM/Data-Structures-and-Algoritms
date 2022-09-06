e42e676ee268de0fec470136d08d31f131a7a1a5
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-09-06 13:52:15 +0530

    Update 0705-designHashSet.cc

--- leetcodeSolutions/0705-designHashSet.cc
+++ leetcodeSolutions/0705-designHashSet.cc
@@ -1,20 +1,44 @@
 class MyHashSet {
 public:
-    vector<bool> hash_set;
-    MyHashSet(): hash_set(1e6 + 1, false) {
+    MyHashSet() {
+      se.resize(BUCKETS);
     }
     
-    void add(int key) {
-      hash_set[key] = true;
+    void add(int key) {      
+      if(contains(key)) {
+        return ;
+      }
+      
+      int hash = hash_fun(key);
+      se[hash].push_back(key);
     }
     
     void remove(int key) {
-      hash_set[key] = false;
+      if(!contains(key)) {
+        return ;
+      }
+      
+      int hash = hash_fun(key);
+      se[hash].erase(search(key));
     }
     
     bool contains(int key) {
-      return hash_set[key];
+      int hash = hash_fun(key);
+      return search(key) != se[hash].end();
     }
+private:
+  // we use chaining here, when get a collision, we could have also used linear probing or quadratic probing
+  vector<list<int>> se;
+  const int BUCKETS = 1e2;
+
+  int hash_fun (int key) {
+      return key % BUCKETS;
+  }
+  
+  list<int>::iterator search(int key) {
+    int hash = hash_fun(key);
+    return find(se[hash].begin(), se[hash].end(), key);
+  }
 };
 
 /**
