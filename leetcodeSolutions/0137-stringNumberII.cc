395431163f48fce90294ee99b2262f4bd505d245
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-09-19 23:02:15 +0530

    Update 0137-stringNumberII.cc

--- leetcodeSolutions/0137-stringNumberII.cc
+++ leetcodeSolutions/0137-stringNumberII.cc
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        vector<int> cnt(32, 0);
-        for(auto &num : nums){
-            for(int bit = 0; bit < 32; ++bit){
-              cnt[bit] += num & 1;
-              num >>= 1;
-            }
-        }
-        
-        int ans = 0;
-        for(int i = 0; i < 32; ++i){
-            if(cnt[i] % 3 != 0) {
-              ans += (1 << i);
-            }
-        }
-        
-        return ans;
+      constexpr int MAX_BITS = 32;
+      
+      vector<int> cnt(MAX_BITS);
+      for(auto &num : nums){
+          for(int bit = 0; bit < MAX_BITS; ++bit){
+            cnt[bit] += num & 1;
+            num >>= 1;
+          }
+      }
+
+      int ans = 0;
+      for(int i = 0; i < MAX_BITS; ++i){
+          if(cnt[i] % 3 != 0) {
+            ans += (1 << i);
+          }
+      }
+
+      return ans;
     }
 };
