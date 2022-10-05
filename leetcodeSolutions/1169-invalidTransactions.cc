721e1b2def9c79e4d22fa829f18f65dfe4ba632a
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-10-05 22:09:42 +0530

    Update 1169-invalidTransactions.cc

--- leetcodeSolutions/1169-invalidTransactions.cc
+++ leetcodeSolutions/1169-invalidTransactions.cc
@@ -3,17 +3,14 @@ public:
     vector<string> invalidTransactions(vector<string>& transactions) {
         unordered_map<string, vector<tuple<int, string, int>>> mp;
         unordered_set<int> se;
-
-        vector<string> ans;
         for(int i = 0; i < transactions.size(); ++i) {
-            auto transaction = transactions[i];
-
-            stringstream sstr(transaction);
+            stringstream sstr(transactions[i]);
             
             string substr;
-            vector<string> transaction_state;
+            vector<string> transaction_state(4);
+            int iter = 0;
             while(getline(sstr, substr, ',')) {
-                transaction_state.push_back(substr);
+                transaction_state[iter++] = substr;
             }
             
             string name = transaction_state[0];
@@ -21,30 +18,24 @@ public:
             int amount = stoi(transaction_state[2]);
             string city = transaction_state[3];
 
+            // invalid amount
             if(amount > 1000) {
                 se.insert(i);
+            }
+
+            // checking with other cities
+            auto &con = mp[name];
+            for(int j = 0; j < con.size(); ++j) {
+                if(abs(get<0>(con[j]) - time) <= 60 && get<1>(con[j]) != city) {
+                    se.insert(i);
+                    se.insert(get<2>(con[j]));
+                }
             }
 
             mp[name].push_back(make_tuple(time, city, i));
         }
 
-        for(auto &[_, con] : mp) {
-            sort(con.begin(), con.end());
-
-            int n = con.size();
-
-            for(int i = 0; i < n; ++i) {
-                // cout << get<0>(con[i]) << ' ' << get<1>(con[i]) << ' ' << get<2>(con[i]) << '\n';
-                for(int j = i + 1; j < n; ++j) {
-                    if(get<0>(con[j]) - get<0>(con[i]) <= 60 && get<1>(con[j]) != get<1>(con[i])) {
-                        se.insert(get<2>(con[i]));
-                        se.insert(get<2>(con[j]));
-                    }
-                }
-            }
-
-        }
-        
+        vector<string> ans;
         for(auto &x : se) {
             ans.push_back(transactions[x]);
         }
