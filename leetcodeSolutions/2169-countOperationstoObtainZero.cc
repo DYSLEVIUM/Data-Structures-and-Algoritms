b6217f3cd78e2714a42d6bae81f4e4f79914d33e
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-10-08 16:48:45 +0530

    Create 2169-countOperationstoObtainZero.cc

---
+++ leetcodeSolutions/2169-countOperationstoObtainZero.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int cnt = 0;
+        while(num1 && num2) {
+            ++cnt;
+            num1 > num2 ? num1 -= num2 : num2 -= num1;
+        }
+
+        return cnt;
+    }
+};
