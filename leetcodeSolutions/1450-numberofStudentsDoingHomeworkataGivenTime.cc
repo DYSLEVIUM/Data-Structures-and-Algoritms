373606e0c4a8cd31222fb9d6b07df7c249d1209d
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-10-08 16:22:10 +0530

    Update 1450-numberofStudentsDoingHomeworkataGivenTime.cc

--- leetcodeSolutions/1450-numberofStudentsDoingHomeworkataGivenTime.cc
+++ leetcodeSolutions/1450-numberofStudentsDoingHomeworkataGivenTime.cc
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int n = startTime.size();
+        int ans = 0;
+        for(int i = 0; i < n; ++i) {
+            ans += startTime[i] <= queryTime && queryTime <= endTime[i];  
+        }
+
+        return ans;
+    }
+};
+
+class Solution1 {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
         constexpr int total_time = 1002;
         static_assert(total_time == 1002);
 
