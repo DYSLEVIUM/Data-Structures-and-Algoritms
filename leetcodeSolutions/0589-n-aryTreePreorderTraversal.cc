0339bf916cd96348ccbba3798f302775ee12e0b7
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-10-08 16:15:13 +0530

    Create 0589-n-aryTreePreorderTraversal.cc

---
+++ leetcodeSolutions/0589-n-aryTreePreorderTraversal.cc
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> ans;
+        auto dfs = [&](const auto &dfs, Node * const node){
+            if(!node) {
+                return ;
+            }
+        
+            ans.push_back(node->val);
+
+            for(auto &child : node->children) {
+                dfs(dfs, child);
+            }
+        };
+        dfs(dfs, root);
+
+        return ans;
+    }
+};
