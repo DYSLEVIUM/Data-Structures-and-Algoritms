35f9b89f575418b47f3d7b73b147ed438ef6900a
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-10-08 05:54:51 +0530

    Create 1502-canMakeArithmeticProgressionFromSequence.cc

---
+++ leetcodeSolutions/1502-canMakeArithmeticProgressionFromSequence.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        int n = arr.size();
+        if(n <= 2) {
+            return true;
+        }
+        
+        sort(arr.begin(), arr.end());
+        
+        int d = arr[1] - arr[0];
+        for(int i = 2; i < n; ++i) {
+            if(arr[i] - arr[i - 1] != d) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
