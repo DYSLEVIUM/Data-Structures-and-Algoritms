69b078ebce9b0a8641ed2d5dc5a9e207828334dd
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-10-09 09:42:37 +0530

    Update 2432-theEmployeeThatWorkedontheLongestTask.cc

--- leetcodeSolutions/2432-theEmployeeThatWorkedontheLongestTask.cc
+++ leetcodeSolutions/2432-theEmployeeThatWorkedontheLongestTask.cc
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int hardestWorker(int n, vector<vector<int>>& logs) {
-      int id = INT_MAX, maxx = INT_MIN, prev = 0;
-      for(auto &log : logs) {
-        int time = log[1] - prev;
-        if(time >= maxx) {
-          if(time == maxx) {
-            id = min(id, log[0]);
-          } else {
-            id = log[0];
-          }
-          maxx = max(maxx, time);
+        int id = INT_MAX, maxx = INT_MIN, prev = 0;
+        for(auto &log : logs) {
+            int time = log[1] - prev;
+            if(time > maxx || (time == maxx && log[0] < id)) {
+                id = log[0];
+                maxx = time;
+            }
+            prev = log[1];
         }
-        prev = log[1];
-      }
       
       return id;
     }
