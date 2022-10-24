aa247234351efca775312d95f0d8a8410b2fedab
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-10-24 07:44:12 +0530

    Update 1239-maximumLengthofaConcatenatedStringwithUniqueCharacters.cc

--- leetcodeSolutions/1239-maximumLengthofaConcatenatedStringwithUniqueCharacters.cc
+++ leetcodeSolutions/1239-maximumLengthofaConcatenatedStringwithUniqueCharacters.cc
@@ -5,17 +5,17 @@ public:
         auto dfs = [&](const auto & dfs, const int & idx, const int & mask){
             if(idx == arr.size()) return 0;
 
-            if(dp.count(idx) && dp[idx].count(mask)) return dp[idx][mask];
+            if(dp.count(mask) && dp[mask].count(idx)) return dp[mask][idx];
 
             int not_take_mask = mask;
             int take_mask = mask;
 
             for(auto & ch : arr[idx]) {
-                if(mask >> (ch - 'a') & 1) return dfs(dfs, idx + 1, not_take_mask);
+                if(take_mask >> (ch - 'a') & 1) return dp[mask][idx] = dfs(dfs, idx + 1, not_take_mask);
                 take_mask |= 1 << (ch - 'a');
             }
 
-            return dp[idx][mask] = max(dfs(dfs, idx + 1, not_take_mask), dfs(dfs, idx + 1, take_mask) + (int) arr[idx].size());
+            return dp[mask][idx] = max(dfs(dfs, idx + 1, not_take_mask), dfs(dfs, idx + 1, take_mask) + (int) arr[idx].size());
         };
         return dfs(dfs, 0, 0);
     }
