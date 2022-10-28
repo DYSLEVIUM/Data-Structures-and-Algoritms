e8f37513b943b46c67d4d03ec4a03c546a585776
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-10-28 10:10:43 +0530

    Update 0049-groupAnagrams.cc

--- leetcodeSolutions/0049-groupAnagrams.cc
+++ leetcodeSolutions/0049-groupAnagrams.cc
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-      unordered_map<string, vector<string>> mp;
-      
-      for(auto& str: strs){
-        string te = str;
-        sort(te.begin(), te.end());
-        
-        mp[te].push_back(str);
-      }
-      
-      vector<vector<string>> ans;
-      for(auto& ana: mp) ans.push_back(ana.second);
-      
-      return ans;
+        unordered_map<string, vector<int>> mp;
+        for(int i = 0; i < strs.size(); ++i) {
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            mp[temp].push_back(i);
+        }
+
+        vector<vector<string>> ans;
+        for(auto & [_, vec] : mp) {
+            vector<string> curr_ans;
+            for(int i : vec) curr_ans.push_back(strs[i]);
+            ans.push_back(curr_ans);
+        }
+
+        return ans;
     }
 };
