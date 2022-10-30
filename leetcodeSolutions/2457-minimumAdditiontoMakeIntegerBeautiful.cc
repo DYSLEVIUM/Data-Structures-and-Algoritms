7b02803e69862f216b2c81f016007ed7136aa827
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-10-31 02:24:51 +0530

    Update 2457-minimumAdditiontoMakeIntegerBeautiful.cc

--- leetcodeSolutions/2457-minimumAdditiontoMakeIntegerBeautiful.cc
+++ leetcodeSolutions/2457-minimumAdditiontoMakeIntegerBeautiful.cc
@@ -11,9 +11,10 @@ public:
             return res;
         };
       
+        // greedily choose the back digits to become 0
         long long temp = n, base = 1;
         while (sum(n) > target) {
-            n = n / 10 + 1;
+            n = n / 10 + 1; // 1 is the forwarded carry to previous digit, i.e., after adding that digit, we make it 0, so the digit just on the left will increment by 1
             base *= 10;
         }
         return n * base - temp;
