1fd7991b8b322aa042ad0c602de0d7a9a48eae19
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-11-01 18:45:22 +0530

    Create 0948-bagofTokens.cc

---
+++ leetcodeSolutions/0948-bagofTokens.cc
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        int n = tokens.size();
+        sort(tokens.begin(), tokens.end());
+
+        int score = 0, maxx = 0;
+        int lo = 0, ri = n - 1;
+        while(lo <= ri) {
+            if(power >= tokens[lo]) {
+                power -= tokens[lo++];
+                ++score;
+            } else {
+                if(score > 0) { // score can't go to negative
+                    power += tokens[ri];
+                    --score;
+                }
+                --ri;
+            }
+            maxx = max(maxx, score);
+        }
+
+        return maxx;
+    }
+};
