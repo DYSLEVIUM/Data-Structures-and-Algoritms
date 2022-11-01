64beaee84c22251d3467f690a44328af6038b8bc
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-11-01 12:06:18 +0530

    Update 1706-whereWilltheBallFall.cc

--- leetcodeSolutions/1706-whereWilltheBallFall.cc
+++ leetcodeSolutions/1706-whereWilltheBallFall.cc
@@ -3,13 +3,16 @@ public:
     vector<int> findBall(vector<vector<int>>& grid) {
         int rows = grid.size(), cols = rows ? grid[0].size() : 0;
 
+        vector<vector<int>> dp(rows, vector<int>(cols, -2)); // -1 represents not reachable, -2 represents not cached
         auto dfs = [&](const auto & dfs, const int & row, const int & col){
             if(row == rows) return col;
 
-            if(col + 1 < cols && grid[row][col] == 1 && grid[row][col + 1] == 1) return dfs(dfs, row + 1, col + 1);
-            if(col - 1 >= 0 && grid[row][col] == -1 && grid[row][col - 1] == -1) return dfs(dfs, row + 1, col - 1);
+            if(dp[row][col] != -2) return dp[row][col];
+
+            if(col + 1 < cols && grid[row][col] == 1 && grid[row][col + 1] == 1) return dp[row][col] = dfs(dfs, row + 1, col + 1);
+            if(col - 1 >= 0 && grid[row][col] == -1 && grid[row][col - 1] == -1) return dp[row][col] = dfs(dfs, row + 1, col - 1);
             
-            return -1; 
+            return dp[row][col] = -1;
         };
 
         vector<int> ans(cols);
