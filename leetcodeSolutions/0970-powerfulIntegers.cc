0f369c6fbefc5ee2f3d39d61fb055b19fbbad592
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-11-05 00:58:21 +0530

    Update 0970-powerfulIntegers.cc

--- leetcodeSolutions/0970-powerfulIntegers.cc
+++ leetcodeSolutions/0970-powerfulIntegers.cc
@@ -2,11 +2,13 @@ class Solution {
 public:
     vector<int> powerfulIntegers(int x, int y, int bound) {
         unordered_set<int> ans;
-        for(int i = 0; i < 20; ++i) {
-            for(int j = 0; pow(x, i) + pow(y, j) < INT_MAX && j < 20; ++j) {
+        for(int i = 0; i < 19; ++i) {
+            for(int j = 0; pow(x,i) + pow(y, j) < INT_MAX; ++j) {
                 int curr = pow(x,i) + pow(y, j);
                 if(curr <= bound) ans.insert(curr);
+                if(y == 1) break;
             }
+            if(x == 1) break;
         }
         return vector<int>(ans.begin(), ans.end());
     }
