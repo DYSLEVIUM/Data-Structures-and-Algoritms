043574b9cf36d0911d054756d2b86951776c8d3f
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-11-05 23:36:25 +0530

    Update 1685-sumofAbsoluteDifferencesinaSortedArray.cc

--- leetcodeSolutions/1685-sumofAbsoluteDifferencesinaSortedArray.cc
+++ leetcodeSolutions/1685-sumofAbsoluteDifferencesinaSortedArray.cc
@@ -3,13 +3,14 @@ public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
         int n = nums.size();
         
-        vector<int> pref = nums;
-        for(int i = 1; i < n; ++i) pref[i] += pref[i - 1];
-
+        // |a - b| = +-(a - b) => max(a - b, b - a)
+        // as it is sorted, all numbers in front of i are small and all the other on the right are large
+        int sum = 0, total = accumulate(nums.begin(), nums.end(), 0);
         vector<int> ans(n);
         for(int i = 0; i < n; ++i) {
-            int pre_sum = (i + 1) * nums[i] - pref[i];
-            int suf_sum = (pref[n - 1] - pref[i]) - (n - 1 - i) * nums[i];
+            sum += nums[i];
+            int pre_sum = (i + 1) * nums[i] - sum;
+            int suf_sum = (total - sum) - (n - 1 - i) * nums[i];
             ans[i] = pre_sum + suf_sum;
         }
 
