cd2a9ce344514485f3c129880212f66cf1f2af0d
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-11-13 13:46:39 +0530

    Update 0151-reverseWordsinaString.cc

--- leetcodeSolutions/0151-reverseWordsinaString.cc
+++ leetcodeSolutions/0151-reverseWordsinaString.cc
@@ -1,42 +1,14 @@
 class Solution {
 public:
     string reverseWords(string s) {
-      int n = s.length();
-      reverse(s.begin(), s.end());
-      
-      int ptr_1 = 0, ptr_2 = 0;
-      while(ptr_2 < n) {
-        if(s[ptr_2] == ' ') {
-          reverse(s.begin() + ptr_1, s.begin() + ptr_2);
-          ptr_1 = ptr_2 + 1;
-        }
-        ++ptr_2;
-      }
-      reverse(s.begin() + ptr_1, s.end());
-      
-      auto remove_whitespace = [](string &s){
-        int ptr_1 = 0;
-        //  white space from the front
-        while(s[ptr_1] == ' ') ++ptr_1;
-        s = s.substr(ptr_1, s.length() - ptr_1);
+        stringstream ss(s);
+        vector<string> words;
+        for(string word; ss >> word;) words.push_back(word);
         
-        //  white space from the back
-        ptr_1 = s.length() - 1;
-        while(s[ptr_1] == ' ') --ptr_1;
-        s = s.substr(0, ptr_1 + 1);
-        
-        //  white space in between
-        ptr_1 = 0;
-        while(ptr_1 < s.length()){
-          if(s[ptr_1] == ' ' && ptr_1 + 1 < s.length() && s[ptr_1 + 1] == ' ') {
-            s.erase(s.begin() + ptr_1 + 1);
-          } else {
-            ++ptr_1;
-          }
-        }
-      };
-      remove_whitespace(s);
-      
-      return s;
+        string ans;
+        for(int i = words.size() - 1; i >= 0; --i) ans += words[i] + " ";
+        ans.pop_back();
+
+        return ans;
     }
 };
