efc872c390165d43515e488c5d001a50796f63ce
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-11-14 17:02:12 +0530

    Update 0278-firstBadVersion.cc

--- leetcodeSolutions/0278-firstBadVersion.cc
+++ leetcodeSolutions/0278-firstBadVersion.cc
@@ -4,14 +4,12 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-      int lo = 1, hi = n;
-      while(lo <= hi){
-        int mid = lo + (hi - lo) / 2;
-        
-        if(isBadVersion(mid)) hi = mid - 1;
-        else lo = mid + 1;
-      }
-      
-      return lo;
+        int lo = 1, hi = n;
+        while(lo < hi) {
+            int mid = lo + ((hi - lo) >> 1);
+            if(!isBadVersion(mid)) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
     }
 };
