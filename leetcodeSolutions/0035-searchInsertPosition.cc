7e3bc7df2cf7596de488aed09fbcebd7968e199a
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-11-14 17:14:34 +0530

    Update 0035-searchInsertPosition.cc

--- leetcodeSolutions/0035-searchInsertPosition.cc
+++ leetcodeSolutions/0035-searchInsertPosition.cc
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
+        int lo = 0, hi = nums.size() - 1;
+        while(lo <= hi) {
+            int mid = lo + ((hi - lo) >> 1);
+
+            if(nums[mid] == target) return mid;
+
+            if(nums[mid] < target) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return lo;
+    }
+};
+
+class Solution1 {
+public:
+    int searchInsert(vector<int>& nums, int target) {
         return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
     }
 };
