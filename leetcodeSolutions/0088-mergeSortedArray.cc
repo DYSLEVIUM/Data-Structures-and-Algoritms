85d1308db517b12f8b39fa059e2a7969f29488ce
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-11-15 13:21:07 +0530

    Update 0088-mergeSortedArray.cc

--- leetcodeSolutions/0088-mergeSortedArray.cc
+++ leetcodeSolutions/0088-mergeSortedArray.cc
@@ -1,17 +1,12 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-      int ptr_1 = m-1, ptr_2 = n-1, ptr_3 = m+n-1;
-      
-      while(ptr_1>=0 && ptr_2>=0){
-        if(nums1[ptr_1]>nums2[ptr_2]){
-          nums1[ptr_3--] = nums1[ptr_1--];
-        }else{
-          nums1[ptr_3--] = nums2[ptr_2--];
+        int p1 = m - 1, p2 = n - 1, p3 = m + n - 1;
+        while(p1 >= 0 && p2 >= 0) {
+            if(nums1[p1] > nums2[p2]) nums1[p3--] = nums1[p1--];
+            else nums1[p3--] = nums2[p2--];
         }
-      }
-      
-      while(ptr_1>=0) nums1[ptr_3--] = nums1[ptr_1--];
-      while(ptr_2>=0) nums1[ptr_3--] = nums2[ptr_2--];
+
+        while(p2 >= 0) nums1[p3--] = nums2[p2--];
     }
 };
