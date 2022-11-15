b3aaaca1d276e868102e2cb07f233b1dfe75ac69
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-11-15 13:24:28 +0530

    Update 0977-squaresofaSortedArray.cc

--- leetcodeSolutions/0977-squaresofaSortedArray.cc
+++ leetcodeSolutions/0977-squaresofaSortedArray.cc
@@ -2,10 +2,7 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         multiset<int> se;
-        for(auto &num : nums) {
-            se.insert(num * num);
-        }
-
+        for(auto &num : nums) se.insert(num * num);
         return vector<int>(se.begin(), se.end());
     }
 };
