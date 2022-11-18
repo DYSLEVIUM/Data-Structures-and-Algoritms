f511c9f47ec7dfbc62d9151a05a4a0143bb05a21
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-11-18 12:56:35 +0530

    Update 0036-validSudoku.cc

--- leetcodeSolutions/0036-validSudoku.cc
+++ leetcodeSolutions/0036-validSudoku.cc
@@ -1,42 +1,42 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-      int rows = 9, cols = 9;
-      
-      //  check row
-      for(int row=0;row<rows;++row){
-        set<int> row_nums;
-        for(int col=0;col<cols;++col) {
-          if(board[row][col] == '.') continue;
-          if(row_nums.find(board[row][col]-'0')!=row_nums.end()) return false;
-          row_nums.insert(board[row][col]-'0');
-        }
-      }
-      
-      // check column
-      for(int col=0;col<cols;++col){
-        set<int> col_nums;
-        for(int row=0;row<rows;++row) {
-          if(board[row][col] == '.') continue;
-          if(col_nums.find(board[row][col]-'0')!=col_nums.end()) return false;
-          col_nums.insert(board[row][col]-'0');
-        }
-      }
-      
-      //  check small box
-      for(int row=0;row<rows;row+=3){
-        for(int col=0;col<cols;col+=3){
-          set<int> box_nums;
-          for(int i=row;i<row+3;++i){
-            for(int j=col;j<col+3;++j){
-              if(board[i][j] == '.') continue;
-              if(box_nums.find(board[i][j]-'0')!=box_nums.end()) return false;
-              box_nums.insert(board[i][j]-'0');
+        static constexpr int N = 9;
+
+        // check row
+        for(int row = 0; row < N; ++row) {
+            vector<int> vis(N + 1);
+            for(int col = 0; col < N; ++col) {
+                if(board[row][col] == '.') continue;
+                if(vis[board[row][col] - '0']) return false;
+                vis[board[row][col] - '0'] = 1;
             }
-          }
         }
-      }
-      
-      return true;
+
+        // check col
+        for(int col = 0; col < N; ++col) {
+            vector<int> vis(N + 1);
+            for(int row = 0; row < N; ++row) {
+                if(board[row][col] == '.') continue;
+                if(vis[board[row][col] - '0']) return false;
+                vis[board[row][col] - '0'] = 1;
+            }
+        }
+
+        // check small-box
+        for(int row = 0; row < N; row += 3) {
+            for(int col = 0; col < N; col += 3) {
+                vector<int> vis(N + 1);
+                for(int irow = row; irow < row + 3; ++irow) {
+                    for(int icol = col; icol < col + 3; ++icol) {
+                        if(board[irow][icol] == '.') continue;
+                        if(vis[board[irow][icol] - '0']) return false;
+                        vis[board[irow][icol] - '0'] = 1;
+                    }
+                }
+            }
+        }
+
+        return true;
     }
 };
