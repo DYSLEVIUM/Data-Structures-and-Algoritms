38879cb45dd2f2779de10e16a095f379f8d35da1
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-11-20 13:13:37 +0530

    Update 0203-removeLinkedListElements.cc

--- leetcodeSolutions/0203-removeLinkedListElements.cc
+++ leetcodeSolutions/0203-removeLinkedListElements.cc
@@ -11,31 +11,20 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-      ListNode* prev = nullptr;
-      while(head && head->val == val){
-        prev = head;
-        head = head->next;
-        delete prev;
-        prev = nullptr;
-      }
-      if(!head || !head->next) return head;
-      
-      ListNode *node = head->next;
-      prev = head;
-      
-      while(node){
-        if(node->val == val){
-          ListNode *temp = node;
-          prev->next = node->next;
-          node = node->next;
-          delete temp;
-          temp = nullptr;
-        }else{
-          prev = node;
-          node = node->next;
+        if(!head) return nullptr;
+
+        while(head && head->val == val) head = head->next;
+
+        ListNode * node = head, * prev = nullptr;
+        while(node) {
+            while(node && node->val == val) node = node->next;
+            
+            if(prev) prev->next = node;
+
+            prev = node;
+            if(node) node = node->next;
         }
-      }
-      
-      return head;
+
+        return head;
     }
 };
