8c17e6faa75c397773ec9c8bfeb63ba81b159260
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-12-04 00:00:49 +0530

    Update 0238-productofArrayExceptSelf.cc

--- leetcodeSolutions/0238-productofArrayExceptSelf.cc
+++ leetcodeSolutions/0238-productofArrayExceptSelf.cc
@@ -1,6 +1,24 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> ans(n, 1);
+        for(int i = 1; i < n; ++i) ans[i] = ans[i - 1] * nums[i - 1];
+
+        int suff_prod = 1;
+        for(int i = n - 1; i >= 0; --i) {
+            ans[i] *= suff_prod;
+            suff_prod *= nums[i];
+        }
+
+        return ans;
+    }
+};
+
+class Solution1 {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
       int n = nums.size();
       
       if(n == 1) {
