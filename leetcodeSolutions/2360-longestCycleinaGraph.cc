54d677d079750f98caaba29d4c29848369e17ace
Pushpakant Behera <pushpakantbehera@hotmail.com>
2023-03-26 14:40:30 +0530

    Update 2360-longestCycleinaGraph.cc

--- leetcodeSolutions/2360-longestCycleinaGraph.cc
+++ leetcodeSolutions/2360-longestCycleinaGraph.cc
@@ -1,49 +1,38 @@
 class Solution {
 public:
     int longestCycle(vector<int>& edges) {
-      int n = edges.size();
-      
-      int maxx = -1;
-      
-      vector<bool> vis(n);
-      auto dfs = [&](const auto &dfs, const int &node, vector<int> &curr_stack){
-        if(node == -1) {
-          return;
-        }
-        if(vis[node]) {
-          int cnt = -1; // to detect 1->2->3, 4->3
-          for(int i = 0; i < curr_stack.size(); ++i) {
-            if(curr_stack[i] == node) {
-              cnt = i;
-              break;
+        static const int INF = 0x3f3f3f3f;
+        int n = edges.size();
+
+        int ans = -1; // assuming we have no cycles
+        vector<int> vis(n);
+        auto dfs = [&](const auto & dfs, const int & node, const int & iter, unordered_map<int, int> & mp){
+            if(edges[node] == -1) {
+                return ;
             }
-          }
-          
-          if(cnt == -1) {
-            return;
-          }
-          
-          int cycle_size = curr_stack.size() - cnt;
-          maxx = max(maxx, cycle_size);
-          
-          return;
+
+            if(vis[node]) {
+                if(mp.count(node)) {
+                    ans = max(ans, iter - mp[node]); // all the nodes between are in the cycle
+                }
+                return ;
+            }
+            vis[node] = 1;
+
+            mp[node] = iter; // abusing the fact that there can only be atmost one outgoing edge
+            dfs(dfs, edges[node], iter + 1, mp);
+            mp.erase(node); // remove from the current stack
+        };
+
+        for(int i = 0; i < n; ++i) {
+            if(vis[i]) {
+                continue;
+            }
+
+            unordered_map<int, int> mp;
+            dfs(dfs, i, 0, mp);
         }
-        
-        vis[node] = true;
-        
-        curr_stack.push_back(node);
-        dfs(dfs, edges[node], curr_stack);
-      };
-      
-      
-      for(int i = 0; i < n; ++i) {
-        if(vis[i]) {
-          continue;
-        }
-        vector<int> curr_stack;
-        dfs(dfs, i, curr_stack);
-      }
-      
-      return maxx;
+
+        return ans == 1 ? -1 : ans; // cycle of 1 doesn't make sense
     }
 };
