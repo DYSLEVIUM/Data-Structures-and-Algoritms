ba7ae6484ddc12b6250d531e44f4b1b94e2c64d8
Pushpakant Behera <pushpakantbehera@hotmail.com>
2023-04-02 13:03:44 +0530

    Update 2300-successfulPairsofSpellsandPotions.cc

--- leetcodeSolutions/2300-successfulPairsofSpellsandPotions.cc
+++ leetcodeSolutions/2300-successfulPairsofSpellsandPotions.cc
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+      int n = spells.size();
+
       sort(potions.begin(), potions.end());
       
-      int np = potions.size();
-      int ns = spells.size();
-      vector<int> ans(ns);
-      for(int i = 0; i < ns; ++i) {
-        long long x = ceil(1.0 * success / spells[i]);
-        int idx = lower_bound(potions.begin(), potions.end(), x) - potions.begin();
-        ans[i] = max(0, np - idx);
+      vector<int> ans(n);
+      for(int i = 0; int &spell: spells) {
+        long long search_val = ceil(1.0 * success / spell);
+        int idx = lower_bound(potions.begin(), potions.end(), search_val) - potions.begin();
+        
+        ans[i++] = potions.size() - idx;
       }
       
       return ans;
