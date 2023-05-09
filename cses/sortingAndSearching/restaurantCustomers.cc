022fb22c6c7a085bd8a674965b76ccf487fa91f1
Pushpakant Behera <pushpakantbehera@hotmail.com>
2023-05-09 21:18:46 +0530

    Update restaurantCustomers.cc

--- cses/sortingAndSearching/restaurantCustomers.cc
+++ cses/sortingAndSearching/restaurantCustomers.cc
@@ -1,7 +1,7 @@
 /*
-  DYSLEVIUM's template
-  Date: 06/April/2022
-  Time: 14:12:26
+    DYSLEVIUM's template
+    Date: 09/May/2023
+    Time: 18:56:46
 */
 
 // headers
@@ -21,21 +21,20 @@
 // clang-format off
 // custom functions
 std::mt19937_64 RNG(std::chrono::high_resolution_clock::now().time_since_epoch().count());	// generator for shuffle and other generator which require random numbers
-// modified custom hash to used with templates from https://codeforces.com/blog/entry/62393
+// modified custom hash to be used with templates from https://codeforces.com/blog/entry/62393
 template<typename T>
-class custom_hash {
- public:
-  static uint64_t splitmix64(uint64_t x) {
-    x += 0x9e3779b97f4a7c15;
-    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-    return x ^ (x >> 31);
-  }
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
 
-  size_t operator()(T x) const {
-    static const uint64_t FIXED_RANDOM = RNG();
-    return splitmix64(std::hash<T>()(x) + FIXED_RANDOM);
-  }
+    size_t operator()(T x) const {
+        static const uint64_t FIXED_RANDOM = RNG();
+        return splitmix64(std::hash<T>()(x) + FIXED_RANDOM);
+    }
 };
 
 // aliases
@@ -49,21 +48,26 @@ template <typename key, typename value> using pbds_unordered_map = __gnu_pbds::g
 template <typename T> using pbds_set = __gnu_pbds::tree<T, __gnu_pbds::null_type, std::less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>; // find_by_order, order_of_key
 
 // constants
-constexpr long double EPS(1e-9);
-constexpr long long MOD(1e9 + 7);  //  or (119 << 23) + 1; primitive_root = 3; // = 998244353
-constexpr long double PI(3.14159265358979323846);
+static constexpr long double EPS(1e-9);
+static constexpr long long MOD(1e9 + 7);  //  or (119 << 23) + 1; primitive_root = 3; // = 998244353
+static constexpr long long INF(0x3f3f3f3f3f3f3f3f);
+static constexpr long double PI(3.14159265358979323846);
 
 // macros
 #define pb push_back
+#define eb emplace_back
 #define F first
 #define S second
 #define fo(i, n) for (ll i = 0; i < (ll)n; ++i)
 #define Fo(i, k, n) for (ll i = k; k < (ll)n ? i < (ll)n : i > (ll)n; k < (ll)n ? ++i : --i)
+#define tr(it, cont) for (auto it = cont.begin(); it != cont.end(); ++it)
 #define all(x) x.begin(), x.end()
 #define ps(x, y) std::fixed << std::setprecision(y) << x
 #define set_bits(x) __builtin_popcountll(x)
 #define zero_bits(x) __builtin_ctzll(x)
 #define mk(arr, n, type) type* arr = new type[n]
+#define low(x) char(x | ' ')
+#define upp(x) char(x & '_')
 
 // template functions
 template <typename T> inline T mod_add(const T& a, const T&  b, const T& mod) { return a + b > mod ? a + b - mod : (a + b); }
@@ -76,65 +80,68 @@ template <typename T> inline T mod_div(const T& a, const T& b, const T& mod) { r
 
 // operator overloading
 template<typename T> std::istream& operator>>(std::istream &is, std::vector<T> &v){ for (auto &it : v) is >> it; return is; }
-template<typename T> std::ostream& operator<<(std::ostream &os, const std::vector<T> &v) { for (auto &it : v) os << it << ' '; return os; }
+template<typename T> std::ostream& operator<<(std::ostream &os, const std::vector<T> &v) { for (const auto &it : v) os << it << ' '; return os; }
 
 template<typename T_1, typename T_2> std::istream& operator>>(std::istream &is, std::pair<T_1, T_2> &p){ is >> p.first >> p.second; return is; }
 template<typename T_1, typename T_2> std::ostream& operator<<(std::ostream &os, const std::pair<T_1, T_2> &p) { os << p.first << ' ' << p.second; return os; }
 
 // debuging
 #ifdef DYSLEVIUM
-  #define deb(x) std::cerr << #x << " = " << x << '\n'
+    #define deb(x) std::cerr << #x << " = " << x << '\n'
 #else
-  #define deb(x)
+    #define deb(x)
 #endif
 
 inline void solve();
 
 inline void setup(){
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(nullptr);
+    // std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr)->sync_with_stdio(false);
 }
 
 int main(int argc, char* argv[]) {
-  setup();
+    setup();
 
-  auto startTime = std::chrono::high_resolution_clock::now();
+    auto startTime = std::chrono::high_resolution_clock::now();
 
-  ll t = 1;
-  // std::cin >> t;
+    ll t = 1;
+    // std::cin >> t;
 
-  while (t--) solve();
+    while (t--) solve();
 
-  auto endTime = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
 
-  #ifdef DYSLEVIUM
-    std::cerr << "Time: " << duration.count();
-  #endif
+    #ifdef DYSLEVIUM
+        std::cerr << "\nTime: " << duration.count();
+    #endif
 
-  return 0;
+    return 0;
 }
 
 using namespace std;
 
 // clang-format on
 inline void solve() {
-  ll n;
-  cin >> n;
+    ll n;
+    cin >> n;
 
-  vpl time(2 * n);
-  fo(i, 2 * n) {
-    cin >> time[i].F >> time[i + 1].F;
-    time[i].S = 1;
-    time[++i].S = -1;
-  }
-  sort(all(time));
+    vpl time(2 * n);
+    fo(i, 2 * n) {
+        cin >> time[i].F >> time[i + 1].F;
+        time[i].S = 1;
+        time[i + 1].S = -1;
+        ++i;
+    }
 
-  ll curr_ans = 0, maxx = 0;
-  fo(i, 2 * n) {
-    curr_ans += time[i].S;
-    maxx = max(curr_ans, maxx);
-  }
+    sort(all(time));
 
-  cout << maxx << '\n';
+    // line-sweep algorithm
+    ll ans = -0x3f3f3f3f, curr = 0;
+    fo(i, 2 * n) {
+        curr += time[i].S;
+        ans = max(ans, curr);
+    }
+
+    cout << ans;
 }
