7d91df7c830431861ee9a67dbbbb1748414dfb33
Pushpakant Behera <pushpakantbehera@hotmail.com>
2023-10-10 12:28:35 +0530

    Create 2009-minimumNumberofOperationstoMakeArrayContinuous.cc

---
+++ leetcodeSolutions/2009-minimumNumberofOperationstoMakeArrayContinuous.cc
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+
+        set<int> se(nums.begin(), nums.end());
+        vector<int> unique(se.begin(), se.end());
+
+        int ans = 0x3f3f3f;
+        // binary search ans
+        // for(int i = 0; i < unique.size(); ++i) {
+        //     int left = unique[i];
+        //     int right = left + n - 1;
+
+        //     int right_pos = upper_bound(unique.begin(), unique.end(), right) - unique.begin();
+
+        //     int count = right_pos - i; // we have this much elements in the middle, so we need to change the other numbers
+        //     ans = min(ans, n - count);
+        // }
+
+        // sliding window answer
+        int right_pos = 0;
+        for(int i = 0; i < unique.size(); ++i) {
+            while(right_pos < unique.size() && unique[i] + n > unique[right_pos]) {
+                ++right_pos;
+            }
+
+            int count = right_pos - i;
+            ans = min(ans, n - count);
+        }
+
+        return ans;
+    }
+};
