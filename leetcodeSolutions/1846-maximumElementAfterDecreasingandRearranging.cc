09486741fdf48234b5447752e6e69fea18b46bc5
Pushpakant Behera <pushpakantbehera@hotmail.com>
2023-11-15 08:08:09 +0530

    Update 1846-maximumElementAfterDecreasingandRearranging.cc

--- leetcodeSolutions/1846-maximumElementAfterDecreasingandRearranging.cc
+++ leetcodeSolutions/1846-maximumElementAfterDecreasingandRearranging.cc
@@ -1,17 +1,36 @@
+// class Solution {
+// public:
+//     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+//         sort(arr.begin(), arr.end());
+//         arr[0] = 1;
+
+//         for(int i = 1; i < arr.size(); ++i) {
+//             if(arr[i] - arr[i - 1] <= 1) {
+//                 continue;
+//             }
+
+//             arr[i] = arr[i - 1] + 1;
+//         }
+
+//         return arr.back();
+//     }
+// };
+
 class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        arr[0] = 1;
+        int n = arr.size();
+        vector<int> cnts(n + 1);
 
-        for(int i = 1; i < arr.size(); ++i) {
-            if(arr[i] - arr[i - 1] <= 1) {
-                continue;
-            }
-
-            arr[i] = arr[i - 1] + 1;
+        for(const int & num : arr) {
+            ++cnts[min(num, n)];
         }
 
-        return arr.back();
+        int ans = 1;
+        for(int num = 2; num <= n; ++num) {
+            ans = min(ans + cnts[num], num); // we cannot exceed num;
+        }
+
+        return ans;
     }
 };
