902db4561ea05c03175bd7a68ed6b863cc29717c
Pushpakant Behera <pushpakantbehera@hotmail.com>
2023-11-20 11:32:22 +0530

    Update 2391-minimumAmountofTimetoCollectGarbage.cc

--- leetcodeSolutions/2391-minimumAmountofTimetoCollectGarbage.cc
+++ leetcodeSolutions/2391-minimumAmountofTimetoCollectGarbage.cc
@@ -1,36 +1,86 @@
+// class Solution {
+// public:
+//     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+//         cin.tie(nullptr)->sync_with_stdio(false);
+
+//         int n = garbage.size();
+
+//         vector<unordered_map<char, int>> suff(n);
+//         for(int i = n - 1; i >= 0; --i) {
+//             for(const char & type: garbage[i]) {
+//                 ++suff[i][type];
+//             }
+
+//             for(const char & type: "MPG") {
+//                 suff[i][type] += i + 1 >= n ? 0 : suff[i + 1][type];
+//             }
+//         }
+
+//         int ans = 0;
+//         for(int i = 0; i < n; ++i) {
+//             int total_trucks = (!!suff[i]['M']) + (!!suff[i]['P']) + (!!suff[i]['G']); // if we don't need the trucks to travel, we won't
+//             ans += total_trucks * (i - 1 >= 0 ? travel[i - 1] : 0) + garbage[i].size();
+//         }
+
+//         return ans;
+//     }
+// };
+
+// class Solution {
+// public:
+//     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+//         cin.tie(nullptr)->sync_with_stdio(false);
+
+//         int n = garbage.size();
+
+//         for(int i = 1; i < travel.size(); ++i) {
+//             travel[i] += travel[i - 1];
+//         }
+
+//         // space is O(1)
+//         unordered_map<char, pair<int, int>> mp; // type -> time and cnt;
+
+//         for(int i = 0; i < n; ++i) {
+//             for(const char & type: garbage[i]) {
+//                 mp[type].first = i - 1 < 0 ? 0 : travel[i - 1];
+//                 mp[type].second += 1;
+//             }
+//         }
+
+//         int ans = 0;
+//         for(const auto & [_, time_cnt]: mp) {
+//             ans += time_cnt.first + time_cnt.second;
+//         }
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        cin.tie(nullptr)->sync_with_stdio(false);
+
         int n = garbage.size();
 
-        unordered_map<char, vector<int>> mp{
-            {'M', vector<int>(n)}, {'P', vector<int>(n)}, {'G', vector<int>(n)}
-        };
-        for(int i = n - 1; i >= 0; --i) {
-            for(auto &ch: garbage[i]) {
-                ++mp[ch][i];
-            }
+        for(int i = 1; i < travel.size(); ++i) {
+            travel[i] += travel[i - 1];
         }
 
-        // made suffix array for each trash
-        for(auto &[ch, trash] : mp) {
-            for(int i = n - 2; i >= 0; --i) {
-                trash[i] += trash[i + 1];
+        int ans = 0;
+
+        unordered_map<char, int> last_pos;
+        for(int i = 0; i < n; ++i) {
+            for(const char & type : garbage[i]) {
+                last_pos[type] = i;
             }
+            ans += garbage[i].size();
         }
 
-        int time = 0;
-        for(auto &[ch, trash] : mp) {
-            time += trash[0]; // collecting all the trash
-            for(int i = 1; i < n; ++i) {
-                // no trash left in the rest of the houses
-                if(!trash[i]) {
-                    break;
-                }
-                time += travel[i - 1];
-            }
+        for(const char & type : "MPG") {
+            ans += last_pos[type] == 0 ? 0 : travel[last_pos[type] - 1];
         }
 
-        return time;
+        return ans;
     }
 };
