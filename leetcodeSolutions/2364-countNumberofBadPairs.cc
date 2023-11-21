4c17f8345ea910380041cccf0e2c8d86d4d5ec30
Pushpakant Behera <pushpakantbehera@hotmail.com>
2023-11-21 11:29:17 +0530

    Update 2364-countNumberofBadPairs.cc

--- leetcodeSolutions/2364-countNumberofBadPairs.cc
+++ leetcodeSolutions/2364-countNumberofBadPairs.cc
@@ -1,18 +1,37 @@
+// class Solution {
+// public:
+//     long long countBadPairs(vector<int>& nums) {
+//         cin.tie(nullptr)->sync_with_stdio(false);
+
+//         int n = nums.size();
+
+//         long long ans = 0;
+//         unordered_map<int, int> mp;
+//         for(int i = 0; i < n; ++i) {
+//             int key = i - nums[i];
+//             ans += mp[key]++;
+//         }
+
+//         long long total_pairs = ((n * (n - 1LL)) >> 1); // nC2
+
+//         return total_pairs - ans;
+//     }
+// };
+
 class Solution {
 public:
     long long countBadPairs(vector<int>& nums) {
+        cin.tie(nullptr)->sync_with_stdio(false);
+
         int n = nums.size();
 
         long long ans = 0;
         unordered_map<int, int> mp;
         for(int i = 0; i < n; ++i) {
-            // i is the total elements till now
-            ans += i - mp[i - nums[i]];
-            
-            // adding new element
-            ++mp[i - nums[i]];
+            int key = i - nums[i];
+            ans += i - mp[key]++; // i is total pairs till this iteration
         }
-        
+
         return ans;
     }
 };
