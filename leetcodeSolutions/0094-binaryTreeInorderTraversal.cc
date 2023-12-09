7edc1027af0d23a599d3bb0b47310cae329085c7
Pushpakant Behera <pushpakantbehera@hotmail.com>
2023-12-09 10:30:50 +0530

    Update 0094-binaryTreeInorderTraversal.cc

--- leetcodeSolutions/0094-binaryTreeInorderTraversal.cc
+++ leetcodeSolutions/0094-binaryTreeInorderTraversal.cc
@@ -9,30 +9,61 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         cin.tie(nullptr)->sync_with_stdio(false);
+
+//         vector<int> ans;
+
+//         TreeNode * curr = root;
+//         stack<TreeNode *> st;
+//         while(curr || !st.empty()) {
+//             while(curr) {
+//                 st.push(curr);
+//                 curr = curr->left;
+//             }
+
+//             curr = st.top();
+//             st.pop();
+
+//             ans.push_back(curr->val);
+
+//             curr = curr->right;
+//         }
+
+//         return ans;
+//     }
+// };
+
+// morris traversal
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        if(root==nullptr) return vector<int>(0);
-        
+        cin.tie(nullptr)->sync_with_stdio(false);
+
         vector<int> ans;
-        
-        TreeNode* curr = root;
-        stack<TreeNode*> st;
-        
-        while(curr!=nullptr || !st.empty()){     
-            while(curr!=nullptr){
-                st.push(curr);
-                curr = curr->left;
+
+        // threaded binary tree
+        TreeNode * curr = root;
+        while(curr != nullptr) {
+            if(curr->left == nullptr) {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                TreeNode * pre = curr->left;
+                while(pre->right != nullptr) {
+                    pre = pre->right;
+                }
+                pre->right = curr;
+                
+                TreeNode * te = curr; // to change the left to null
+                curr = curr->left; // update for next iteration
+
+                te->left = nullptr;
             }
-            
-            curr = st.top();
-            st.pop();
-            
-            ans.emplace_back(curr->val);
-            
-            curr = curr->right;
         }
-        
+
         return ans;
     }
 };
