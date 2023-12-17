13922c46adfb1bdc111cdec5a1adaeced48b1b83
Pushpakant Behera <pushpakantbehera@hotmail.com>
2023-12-17 12:31:10 +0530

    Create 2353-designaFoodRatingSystem.cc

---
+++ leetcodeSolutions/2353-designaFoodRatingSystem.cc
@@ -0,0 +1,51 @@
+using PIS = pair<int, string>;
+auto cmp = [](const PIS & lhs, const PIS & rhs){
+    if(lhs.first == rhs.first) {
+        return lhs.second < rhs.second;
+    }
+
+    return lhs.first > rhs.first;
+};
+
+using SPIS = set<PIS, decltype(cmp)>;
+
+class FoodRatings {
+public:
+    FoodRatings(const vector<string> & foods, const vector<string> & cuisines, const vector<int> & ratings) {
+        cin.tie(nullptr)->sync_with_stdio(false);
+
+        int n = foods.size();
+        for(int i = 0; i < n; ++i) {
+            foodToCuisine[foods[i]] = cuisines[i];
+            changeRating(foods[i], ratings[i]);
+        }
+    }
+    
+    void changeRating(const string & food, const int & newRating) {
+        string cuisine = foodToCuisine[food];
+
+        auto oldIter = highestRatedCuisine[cuisine].find({foodRating[food], food});
+        if(oldIter != highestRatedCuisine[cuisine].end()) {
+            highestRatedCuisine[cuisine].erase(oldIter);
+        }
+        
+        foodRating[food] = newRating;
+        highestRatedCuisine[cuisine].insert({newRating, food});
+    }
+    
+    string highestRated(const string & cuisine) {
+        return (*highestRatedCuisine[cuisine].begin()).second;
+    }
+
+private:
+    unordered_map<string, string> foodToCuisine;
+    unordered_map<string, int> foodRating;
+    unordered_map<string, SPIS> highestRatedCuisine;
+};
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
