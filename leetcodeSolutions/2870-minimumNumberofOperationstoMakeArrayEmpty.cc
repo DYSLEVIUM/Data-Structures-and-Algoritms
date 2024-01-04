69ff5c79939177764e014ffdbc164952e9b83f47
Pushpakant Behera <pushpakantbehera@hotmail.com>
2024-01-04 14:15:49 +0530

    Create 2870-minimumNumberofOperationstoMakeArrayEmpty.cc

---
+++ leetcodeSolutions/2870-minimumNumberofOperationstoMakeArrayEmpty.cc
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        cin.tie(nullptr)->sync_with_stdio(false);
+
+        unordered_map<int, int> mp;
+        for(const int & num : nums) {
+            ++mp[num];
+        }
+
+        int ans = 0;
+        for(const auto & [k, v]: mp) {
+            if(v == 1) {
+                return -1;
+            }
+            ans += v / 3 + (v % 3 != 0);
+        }
+
+        return ans;
+    }
+};
