9b970505a2a610b4533113daeb54916ed40bf926
Pushpakant Behera <pushpakantbehera@hotmail.com>
2024-02-02 04:26:58 +0530

    Update 0698-partitiontoKEqualSumSubsets.cc

--- leetcodeSolutions/0698-partitiontoKEqualSumSubsets.cc
+++ leetcodeSolutions/0698-partitiontoKEqualSumSubsets.cc
@@ -1,43 +1,64 @@
+#pragma GCC optimize("O3", "unroll-loops")
+
 class Solution {
 public:
-    bool canPartitionKSubsets(vector<int>& nums, int k) {
-      int n = nums.size();
-      
-      int total = accumulate(nums.begin(), nums.end(), 0);
-      int total_in_set = total / k;
-      
-      //  impossible solution
-      if(n < k || total_in_set * k != total || *max_element(nums.begin(), nums.end()) > total_in_set) {
-        return false;
-      }
-      
-      vector<int> cache(1 << n, -1);
-      auto can_make_set = [&](const auto &can_make_set, const int &sets_left, const int &mask, const int &curr_sum){
-        if(!sets_left) {
-          return true;
+    bool canPartitionKSubsets(const vector<int> & nums, const int & k) {
+        cin.tie(nullptr)->sync_with_stdio(false);
+
+        const int n = nums.size();
+        if (n < k) {
+            return false;
         }
-        if(curr_sum == total_in_set) {
-          return can_make_set(can_make_set, sets_left - 1, mask, 0);
+
+        int sum = 0, maxx = -0x3f3f3f;
+        for (int i = 0; i < n; ++i) {
+            sum += nums[i];
+            maxx = max(maxx, nums[i]);
         }
-        
-        if(cache[mask] != -1) {
-          return !!cache[mask];
+
+        if (sum % k) {
+            return false;
         }
-        
-        for(int i = 0; i < n; ++i) {
-          //  if the ith number has not been used
-          if((mask & (1 << i)) == 0 && curr_sum + nums[i] <= total_in_set) {
-            if(can_make_set(can_make_set, sets_left, mask | (1 << i), curr_sum + nums[i])) {
-              cache[mask] = true;
-              return true;
-            } 
-          }
+
+        const int block_sum = sum / k;
+        if (maxx > block_sum) {
+            return false;
         }
         
-        cache[mask] = false;
-        return false;
-      };   
-    
-      return can_make_set(can_make_set, k - 1, 0, 0); //  last set is implied
+        vector<int> cache(1 << n, -1);
+        auto can_make_blocks = [&](const auto & can_make_blocks, const int & idx, const int & blocks_left, const int & mask, const int & curr_sum) -> bool {
+            if (!blocks_left) {
+                return true;
+            }
+
+            if (cache[mask] != -1) {
+                return cache[mask];
+            }
+
+            if (idx == n || curr_sum > block_sum) {
+                return false;
+            }
+
+            if (curr_sum == block_sum) {
+                return cache[mask] = can_make_blocks(can_make_blocks, 0, blocks_left - 1, mask, 0);
+            }
+
+            for (int i = idx; i < n; ++i) {
+                const int is_used = mask & (1 << i);
+                if (is_used) {
+                    continue;
+                }
+
+                const int use_num_mask = mask | (1 << i);
+                cache[mask] = can_make_blocks(can_make_blocks, i + 1, blocks_left, use_num_mask, curr_sum + nums[i]);
+                if (cache[mask]) {
+                    return true;
+                }
+            }
+
+            return cache[mask] = false;
+        };
+
+        return can_make_blocks(can_make_blocks, 0, k - 1, 0, 0); // last set is implied
     }
 };
