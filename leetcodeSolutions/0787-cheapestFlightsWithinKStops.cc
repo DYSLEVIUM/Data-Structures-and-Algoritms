831430d6d8659eff9b1d04c355de979db8e7caa8
Pushpakant Behera <pushpakantbehera@hotmail.com>
2024-02-23 19:20:22 +0530

    Update 0787-cheapestFlightsWithinKStops.cc

--- leetcodeSolutions/0787-cheapestFlightsWithinKStops.cc
+++ leetcodeSolutions/0787-cheapestFlightsWithinKStops.cc
@@ -17,6 +17,7 @@ public:
             int node, cost, stops;
         };
 
+        // dijkstra's implementation, O(V²)b better use BFS to find shortest path in O(V+E)
         auto heap_compare = [](const Route & lhs, const Route & rhs){
             return lhs.cost < rhs.cost;
         };
