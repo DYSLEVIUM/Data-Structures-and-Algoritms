399b83991f0fb5b4f22afa81cec331b000845c90
Pushpakant Behera <pushpakantbehera@hotmail.com>
2024-03-02 13:13:36 +0530

    Update 0977-squaresofaSortedArray.cc

--- leetcodeSolutions/0977-squaresofaSortedArray.cc
+++ leetcodeSolutions/0977-squaresofaSortedArray.cc
@@ -1,8 +1,26 @@
+#pragma GCC optimize("O3", "unroll-loops")
+
+auto _ = [](){
+    return cin.tie(nullptr)->sync_with_stdio(false);
+}();
+
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums) {
-        multiset<int> se;
-        for(auto &num : nums) se.insert(num * num);
-        return vector<int>(se.begin(), se.end());
+    vector<int> sortedSquares(const vector<int> & nums) {
+        const int n = nums.size();
+
+        vector<int> ans(n);
+
+        int ptr = n;
+        int st = 0, ed = n - 1;
+        while(ptr--) {
+            if(abs(nums[st]) >= abs(nums[ed])) {
+                ans[ptr] = nums[st] * nums[st++];
+            } else {
+                ans[ptr] = nums[ed] * nums[ed--];
+            }
+        }
+
+        return ans;
     }
 };
