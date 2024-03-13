1f341724d64e2e253dde842b49e6fd17507eb4d6
Pushpakant Behera <pushpakantbehera@hotmail.com>
2024-03-13 18:09:12 +0530

    Update 2485-findthePivotInteger.cc

--- leetcodeSolutions/2485-findthePivotInteger.cc
+++ leetcodeSolutions/2485-findthePivotInteger.cc
@@ -11,3 +11,20 @@ public:
         return x - floor(x) ? -1 : x;
     }
 };
+
+class Solution1 {
+public:
+    int pivotInteger(const int & n) {
+        unsigned long long total = n * (n + 1) / 2;
+        unsigned long long sum = 0;
+        for(int i = 1; i <= n; ++i) {
+            sum += i;
+            unsigned long long other = total - sum + i;
+            if (sum == other) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
