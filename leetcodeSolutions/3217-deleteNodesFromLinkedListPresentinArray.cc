398db42ff6684982d38f231f88289114c7ebe50c
Pushpakant Behera <pushpakantbehera@hotmail.com>
2024-09-07 03:54:03 +0530

    Create 3217-deleteNodesFromLinkedListPresentinArray.cc

---
+++ leetcodeSolutions/3217-deleteNodesFromLinkedListPresentinArray.cc
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+#pragma GCC optimize("O3", "unroll-loops")
+
+auto _ = [](){
+    return cin.tie(nullptr)->sync_with_stdio(false);
+}();
+
+class Solution {
+public:
+    ListNode* modifiedList(const vector<int> & nums, const ListNode * const head) {
+        unordered_set<int> nums_se(nums.begin(), nums.end());
+
+        ListNode dummy(0, const_cast<ListNode *>(head));
+
+        ListNode * prev = &dummy;
+        const ListNode * curr = const_cast<ListNode *>(head);
+        while(curr) {
+            const ListNode * next = curr->next;
+
+            if(nums_se.count(curr->val)) {
+                prev->next = const_cast<ListNode *>(next);
+
+                delete curr;
+                curr = nullptr;
+            } else {
+                prev = const_cast<ListNode *>(curr);
+            }
+
+            curr = next;
+        }
+
+        return dummy.next;
+    }
+};
