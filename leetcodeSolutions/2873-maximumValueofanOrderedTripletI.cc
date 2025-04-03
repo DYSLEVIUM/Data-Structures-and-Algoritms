5c394542b9f1786f42fab807970cf99330dbc61b
Pushpakant Behera <pushpakantbehera@hotmail.com>
2025-04-04 02:53:17 +0530

    Update 2873-maximumValueofanOrderedTripletI.cc

--- leetcodeSolutions/2873-maximumValueofanOrderedTripletI.cc
+++ leetcodeSolutions/2873-maximumValueofanOrderedTripletI.cc
@@ -7,15 +7,23 @@ auto _ = [](){
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
+        static constinit const int INF = 0x3f3f3f3f;
+
         const int n = nums.size();
 
-        long long maxx = 0;
+        vector<int> prefix_max(n + 1), suffix_max(n + 1);
+        prefix_max[0] = suffix_max[n] = -INF;
         for(int i = 0; i < n; ++i) {
-            for(int j = i + 1; j < n; ++j) {
-                for(int k = j + 1; k < n; ++k) {
-                    maxx = max(maxx, 1LL * (nums[i] - nums[j]) * nums[k]);
-                }
-            }
+            prefix_max[i + 1] = max(prefix_max[i], nums[i]);
+
+            int inv = n - 1 - i;
+            suffix_max[inv] = max(suffix_max[inv + 1], nums[inv]);
+        }
+
+        long long maxx = 0;
+        for(int j = 1; j < n - 1; ++j) {
+            long long curr = 1LL * (prefix_max[j] - nums[j]) * suffix_max[j + 1];
+            maxx = max(maxx, curr);
         }
 
         return maxx;
