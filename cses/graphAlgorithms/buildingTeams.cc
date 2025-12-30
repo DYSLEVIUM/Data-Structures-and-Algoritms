7dac920bfcb0905b4ef4d310fb2bbb695d02a7e3
Pushpakant Behera <pushpakantbehera@hotmail.com>
2025-12-31 03:13:29 +0530

    Update DYSLEVIUM template and improve formatting

--- cses/graphAlgorithms/buildingTeams.cc
+++ cses/graphAlgorithms/buildingTeams.cc
@@ -1,41 +1,42 @@
 /*
-  DYSLEVIUM's template
-  Date: 22/April/2022
-  Time: 17:27:28
+    DYSLEVIUM's template
+    Date: 31/December/2025
+    Time: 02:50:13
 */
+
+// clang-format off
 
 // headers
 #ifdef DYSLEVIUM
-#include "dyslevium.h"
+    #include "dyslevium.h"
 #else
-// optimizations done at compile time
-#pragma GCC optimize("Ofast,fast-math,unroll-loops")
-#pragma GCC target("avx,avx2,fma,bmi,bmi2,lzcnt,popcnt")
+    // optimizations done at compile time
+    #pragma GCC optimize("Ofast,fast-math,unroll-loops")
+    // #pragma GCC target("avx,avx2,fma,bmi,bmi2,lzcnt,popcnt")
 
-#include <bits/stdc++.h>
+    #include <bits/stdc++.h>
 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+    #include <ext/pb_ds/assoc_container.hpp>
+    #include <ext/pb_ds/tree_policy.hpp>
 #endif
 
-// clang-format off
 // custom functions
 std::mt19937_64 RNG(std::chrono::high_resolution_clock::now().time_since_epoch().count());	// generator for shuffle and other generator which require random numbers
-// modified custom hash to used with templates from https://codeforces.com/blog/entry/62393
-template<typename T>
-class custom_hash {
- public:
-  static uint64_t splitmix64(uint64_t x) {
-    x += 0x9e3779b97f4a7c15;
-    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-    return x ^ (x >> 31);
-  }
 
-  size_t operator()(T x) const {
-    static const uint64_t FIXED_RANDOM = RNG();
-    return splitmix64(std::hash<T>()(x) + FIXED_RANDOM);
-  }
+// modified custom hash to be used with templates from https://codeforces.com/blog/entry/62393
+template <typename T>
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(T x) const {
+        static const uint64_t FIXED_RANDOM = RNG();
+        return splitmix64(std::hash<T>()(x) + FIXED_RANDOM);
+    }
 };
 
 // aliases
@@ -45,116 +46,132 @@ using pl = std::pair<ll, ll>;
 using vl = std::vector<ll>;
 using vvl = std::vector<vl>;
 using vpl = std::vector<pl>;
-template <typename key, typename value> using pbds_unordered_map = __gnu_pbds::gp_hash_table<key, value, custom_hash<key>>; // 3x - 6x faster than stl unordered_map
-template <typename T> using pbds_set = __gnu_pbds::tree<T, __gnu_pbds::null_type, std::less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>; // find_by_order, order_of_key
 
+#ifndef DYSLEVIUM
+    template <typename T>
+    using pbds_ordered_set = __gnu_pbds::tree<T, __gnu_pbds::null_type, std::less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;  // find_by_order, order_of_key
+
+    template <typename T1, typename T2>
+    using pbds_unordered_map = __gnu_pbds::gp_hash_table<T1, T2, custom_hash<T1>>; // 4x - 10x faster than stl unordered_map
+#endif
 // constants
-constexpr long double EPS(1e-9);
-constexpr long long MOD(1e9 + 7);  //  or (119 << 23) + 1; primitive_root = 3; // = 998244353
-constexpr long double PI(3.14159265358979323846);
+static const constexpr long double EPS(1e-9);
+static const constexpr int MOD(1e9 + 7);  //  or (119 << 23) + 1; primitive_root = 3; // = 998244353
+static const constexpr long long INF(0x3f3f3f3f3f3f3f3f);
+static const constexpr long double PI(3.14159265358979323846);
 
 // macros
 #define pb push_back
+#define eb emplace_back
 #define F first
 #define S second
 #define fo(i, n) for (ll i = 0; i < (ll)n; ++i)
 #define Fo(i, k, n) for (ll i = k; k < (ll)n ? i < (ll)n : i > (ll)n; k < (ll)n ? ++i : --i)
+#define tr(it, cont) for (auto it = cont.begin(); it != cont.end(); ++it)
 #define all(x) x.begin(), x.end()
-#define ps(x, y) std::fixed << std::setprecision(y) << x
+#define ps(x) std::fixed << std::setprecision(x)
 #define set_bits(x) __builtin_popcountll(x)
 #define zero_bits(x) __builtin_ctzll(x)
 #define mk(arr, n, type) type* arr = new type[n]
+#define low(x) char(x | ' ')
+#define upp(x) char(x & '_')
+#define sz(x) x.size()
 
 // template functions
-template <typename T> inline T mod_add(const T& a, const T&  b, const T& mod) { return a + b > mod ? a + b - mod : (a + b); }
-template <typename T> inline T mod_sub(const T& a, const T&  b, const T& mod) { return a - b < 0 ? a - b + mod : (a - b); }
-template <typename T> inline T mod_mul(const T& a, const T&  b, const T& mod) { return ((((a % mod) * (b % mod)) % mod) + mod) % mod; };
-template <typename T> inline T bin_pow(T x, T n) {T res = 1; while (n) { if (n & 1) res *= x; x *= x; n >>= 1; } return res; }
-template <typename T> inline T bin_pow_m(T x, T n, const T& mod) {T res = 1; while (n) { if (n & 1) res = mod_mul(res, x, mod); x = mod_mul(x, x, mod); n >>= 1; } return res % mod; }
-template <typename T> inline T mod_inverse(const T& a, const T& mod) { return bin_pow_m(a, mod - 2, mod); }
-template <typename T> inline T mod_div(const T& a, const T& b, const T& mod) { return mod_mul(a, mod_inverse(b, mod), mod); }
+template <typename T> inline constexpr T mod_add(const T& a, const T&  b, const T& mod = MOD) { return a + b >= mod ? a + b - mod : a + b; }
+template <typename T> inline constexpr T mod_sub(const T& a, const T&  b, const T& mod = MOD) { return a - b < 0 ? mod_add(a - b, mod) : a - b; }
+template <typename T> inline constexpr T mod_mul(const T& a, const T&  b, const T& mod = MOD) { return ((((a % mod) * (b % mod)) % mod) + mod) % mod; };
+template <typename T> inline constexpr T bin_pow(T x, T n) {T res = 1; while (n) { if (n & 1) res *= x; x *= x; n >>= 1; } return res; }
+template <typename T> inline constexpr T bin_pow_m(T x, T n, const T& mod = MOD) {T res = 1; while (n) { if (n & 1) res = mod_mul(res, x, mod); x = mod_mul(x, x, mod); n >>= 1; } return res % mod; }
+template <typename T> inline constexpr T mod_inverse(const T& a, const T& mod = MOD) { return bin_pow_m(a, mod - 2, mod); }
+template <typename T> inline constexpr T mod_div(const T& a, const T& b, const T& mod = MOD) { return mod_mul(a, mod_inverse(b, mod), mod); }
 
 // operator overloading
-template<typename T> std::istream& operator>>(std::istream &is, std::vector<T> &v){ for (auto &it : v) is >> it; return is; }
-template<typename T> std::ostream& operator<<(std::ostream &os, const std::vector<T> &v) { for (auto &it : v) os << it << ' '; return os; }
+template <typename T> std::istream& operator>>(std::istream &is, std::vector<T> &v) { for (auto &it : v) is >> it; return is; }
+template <typename T> std::ostream& operator<<(std::ostream &os, const std::vector<T> &v) { for (const auto &it : v) os << it << ' '; return os; }
 
-template<typename T_1, typename T_2> std::istream& operator>>(std::istream &is, std::pair<T_1, T_2> &p){ is >> p.first >> p.second; return is; }
-template<typename T_1, typename T_2> std::ostream& operator<<(std::ostream &os, const std::pair<T_1, T_2> &p) { os << p.first << ' ' << p.second; return os; }
+template <typename T1, typename T2> std::istream& operator>>(std::istream &is, std::pair<T1, T2> &p) { is >> p.first >> p.second; return is; }
+template <typename T1, typename T2> std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) { os << p.first << ' ' << p.second; return os; }
 
-// debuging
+// debugging
 #ifdef DYSLEVIUM
-  #define deb(x) std::cerr << #x << " = " << x << '\n'
+    #define deb(x) std::cerr << #x << " = " << x << '\n'
 #else
-  #define deb(x)
+    #define deb(x)
 #endif
 
 inline void solve();
 
 inline void setup(){
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(nullptr);
+    // std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    #ifndef ONLINE_JUDGE  // runs automatically for supported online judges
+        freopen("input.in", "r", stdin);
+        freopen("output.out", "w", stdout);
+        freopen("error.err", "w", stderr);
+    #endif
 }
 
 int main(int argc, char* argv[]) {
-  setup();
+    setup();
 
-  auto startTime = std::chrono::high_resolution_clock::now();
+    auto startTime = std::chrono::high_resolution_clock::now();
 
-  ll t = 1;
-  // std::cin >> t;
+    ll t = 1;
+    // std::cin >> t;
 
-  while (t--) solve();
+    while (t--) solve();
 
-  auto endTime = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
 
-  #ifdef DYSLEVIUM
-    std::cerr << "Time: " << duration.count();
-  #endif
+    #ifdef DYSLEVIUM
+        std::cerr << "\nTime: " << duration.count() << "ms";
+    #endif
 
-  return 0;
+    return 0;
 }
 
 using namespace std;
 
 // clang-format on
+
 inline void solve() {
-  ll n, m;
-  cin >> n >> m;
+    ll n, m;
+    cin >> n >> m;
 
-  vvl gr(n);
-  fo(i, m) {
-    ll a, b;
-    cin >> a >> b;
+    vvl gr(n);
+    fo(i, m) {
+        ll a, b;
+        cin >> a >> b;
 
-    --a, --b;
-    gr[a].pb(b);
-    gr[b].pb(a);
-  }
-
-  vl ans(n, -1);
-  auto dfs = [&](const auto& dfs, const ll& node, const ll& parent,
-                 const ll& prev) -> bool {
-    if (ans[node] == -1) {
-      ans[node] = 1 ^ 2 ^ prev;
-      for (auto& neighbour : gr[node]) {
-        if (neighbour == parent) continue;
-        if (!dfs(dfs, neighbour, node, ans[node])) return false;
-      }
-      return true;
+        --a, --b;
+        gr[a].eb(b), gr[b].eb(a);
     }
-    if (ans[node] != ans[parent]) return true;
 
-    return false;
-  };
+    vl ans(n, -1);
+    auto dfs = [&](const auto& dfs, const ll& node, const ll& par = -1) {
+        if (ans[node] != -1) {
+            return ans[node] != ans[par];
+        }
 
-  fo(i, n) {
-    if (ans[i] != -1) continue;
-    if (!dfs(dfs, i, -1, 2)) {
-      cout << "IMPOSSIBLE";
-      return;
+        ans[node] = 1 ^ 2 ^ (par == -1 ? 2 : ans[par]);
+        for (const auto& neigh : gr[node]) {
+            if (!dfs(dfs, neigh, node)) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    fo(i, n) {
+        if (!dfs(dfs, i)) {
+            cout << "IMPOSSIBLE";
+            return;
+        }
     }
-  }
 
-  cout << ans;
+    cout << ans;
 }
